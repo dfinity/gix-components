ae9446602d8ff9e7672d343bd417afd362303844
David Dal Busco <david.dalbusco@dfinity.org>
2022-06-22 09:51:59 +0200

    build: sveltekit and storybook

---
+++ src/lib/Demo.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+  export let color = "";
+
+  const dispatch = createEventDispatcher();
+
+  const demo = () => dispatch("nnsDemo");
+</script>
+
+<button on:click={demo} style="background: {color}">Hello I'm a button</button>
---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Demo from "$lib/Demo.svelte";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<Demo color="yellow" on:nnsDemo={() => alert("Demo")} />
