ff2e43eea355db6a091c86dd4be32b263febff44
David Dal Busco <david.dalbusco@outlook.com>
2022-06-27 08:17:57 +0200

    build: init library (#1)
    # Motivation
    
    Init the build and tools we need for the design system.
    
    # Changes
    
    - setup `SvelteKit`
    - setup `Storybook`
    - add `jest` and `testinglibrary` for test purpose
    - add `CODEOWNERS` and PR template for GitHub

---
+++ src/lib/index.ts
@@ -0,0 +1,1 @@
+export { default as Demo } from "./components/Demo.svelte";
---
+++ src/stories/Demo.stories.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import Demo from "../lib/components/Demo.svelte";
+</script>
+
+<Meta
+  title="GIX/Demo"
+  component={Demo}
+  argTypes={{
+    color: { control: "color" },
+  }}
+/>
+
+<Template let:args>
+  <Demo {...args} on:click={args.onClick} />
+</Template>
+
+<Story
+  name="Red"
+  args={{
+    color: "red",
+  }}
+/>
+
+<Story
+  name="Blue"
+  args={{
+    color: "blue",
+  }}
+/>
