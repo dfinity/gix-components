3b47d8357f2901aea8bdb86de24d0b8f276e1979
David Dal Busco <david.dalbusco@dfinity.org>
2022-07-15 11:40:36 +0200

    feat: styles (#2)
    # Motivation
    
    Set up styling
    
    # Changes
    
    - copy global styles from nns-dapp
    - add sass
    - document import in readme

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,16 @@
 import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
+import autoprefixer from 'autoprefixer';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: preprocess(),
+  preprocess: preprocess({
+    postcss: {
+      plugins: [autoprefixer]
+    }
+  }),
 
   kit: {
     adapter: adapter(),
