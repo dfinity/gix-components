ea1a31d092b5e0b9d915a8a7b4c2fac5b23feab9
David Dal Busco <david.dalbusco@dfinity.org>
2022-08-01 16:56:17 +0200

    feat: export new components

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,3 +5,6 @@ export { default as MenuButton } from "./components/MenuButton.svelte";
 export { default as MenuItem } from "./components/MenuItem.svelte";
 export { default as SplitPane } from "./components/SplitPane.svelte";
 export { default as Toolbar } from "./components/Toolbar.svelte";
+export { default as Back } from "./components/Back.svelte";
+export { default as Card } from "./components/Card.svelte";
+export { default as CardGrid } from "./components/CardGrid.svelte";
