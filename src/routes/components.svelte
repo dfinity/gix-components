b1a8fab8fe440db15c6ed93f199e93298fa4e9fd
David Dal Busco <david.dalbusco@dfinity.org>
2022-08-10 12:58:27 +0200

    docs: grid

--- src/routes/components.svelte
+++ src/routes/components.svelte
@@ -1,9 +1,34 @@
+<script lang="ts">
+  import Card from "../lib/components/Card.svelte";
+  import { goto } from "$app/navigation";
+</script>
+
 <h1>Components</h1>
 
-// TODO: card grid and a card per component
+<p>
+  The UI elements that you can inject into your project either by using the
+  components or through styles.
+</p>
 
-<a href="/components/layout">
-  <article>Layout</article>
-</a>
+<div class="grid">
+  <Card role="link" on:click={() => goto("/components/layout")}>
+    <h2 class="title" slot="start">Layout</h2>
 
-// TODO: document "Back", "CardGrid" and "Card"
+    <p>Header, menu and footer. The structure of your dapp.</p>
+  </Card>
+
+  <Card role="link" on:click={() => goto("/components/grid")}>
+    <h2 class="title" slot="start">Grid</h2>
+
+    <p>A grid based columns container to spread content.</p>
+  </Card>
+</div>
+
+<p>TODO docs:</p>
+
+<ul>
+  <li>Card</li>
+  <li>Back, menu, menubutton, menuitem, splitpane (maybe, included in layout)</li>
+  <li>Toolbar (maybe, included in layout)</li>
+  <li>Backdrop (maybe, included in overlays)</li>
+</ul>
