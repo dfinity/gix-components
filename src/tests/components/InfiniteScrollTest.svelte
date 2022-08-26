6f6e0974467000116849344408d16b6f210f9a7d
David Dal Busco <david.dalbusco@dfinity.org>
2022-08-26 13:12:00 +0200

    test: update test for new behavior

--- src/tests/components/InfiniteScrollTest.svelte
+++ src/tests/components/InfiniteScrollTest.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
   import InfiniteScroll from "$lib/components/InfiniteScroll.svelte";
-  import { DEFAULT_LIST_PAGINATION_LIMIT } from "$lib/constants/constants";
 
   export let elements: number[];
-  export let pageLimit: number = DEFAULT_LIST_PAGINATION_LIMIT;
+  export let disabled: boolean = false;
   export let spy: () => void;
 </script>
 
-<InfiniteScroll on:nnsIntersect={spy} {pageLimit}>
+<InfiniteScroll on:nnsIntersect={spy} {disabled}>
   {#each elements as element, i}
     <div>Test {i}</div>
   {/each}
