e3c7c2881cd68f0ec07fb3fead342743564377fc
David Dal Busco <david.dalbusco@dfinity.org>
2022-09-01 18:52:15 +0200

    feat: skeleton text

---
+++ src/lib/components/SkeletonText.svelte
@@ -0,0 +1,69 @@
+<!-- adapted source: https://github.com/ionic-team/ionic-framework/tree/main/core/src/components/skeleton-text -->
+<script lang="ts">
+  export let animated: boolean = true;
+  export let tagName: "span" | "p" | "h1" | "h2" | "h3" = "span";
+</script>
+
+<div
+  data-tid="skeleton-text"
+  aria-busy="true"
+  class="skeleton-text"
+  class:animated
+>
+  <svelte:element this={tagName}>&nbsp;</svelte:element>
+</div>
+
+<style lang="scss">
+  .skeleton-text {
+    display: block;
+
+    width: 100%;
+    height: inherit;
+
+    margin: var(--padding-0_5x) 0;
+
+    --skeleton-text-background: rgba(var(--background-contrast-rgb), 0.065);
+    --skeleton-text-background-animated: rgba(
+      var(--background-contrast-rgb),
+      0.135
+    );
+    background: var(--skeleton-text-background);
+
+    line-height: var(--skeleton-text-line-height, 0.8);
+
+    user-select: none;
+    pointer-events: none;
+
+    * {
+      display: inline-block;
+    }
+  }
+
+  .animated {
+    position: relative;
+
+    background: linear-gradient(
+      to right,
+      var(--skeleton-text-background) 8%,
+      var(--skeleton-text-background-animated) 18%,
+      var(--skeleton-text-background) 33%
+    );
+    background-size: 800px 104px;
+    animation-duration: 1s;
+    animation-fill-mode: forwards;
+    animation-iteration-count: infinite;
+    animation-name: skeleton-text-shimmer;
+    animation-timing-function: linear;
+  }
+
+  /* -global- */
+  @keyframes -global-skeleton-text-shimmer {
+    0% {
+      background-position: -400px 0;
+    }
+
+    100% {
+      background-position: 400px 0;
+    }
+  }
+</style>
