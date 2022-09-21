40d2635538436aa271777d92912472e0a609f37d
David Dal Busco <david.dalbusco@dfinity.org>
2022-09-21 08:46:36 +0200

    feat: in case of errors display toasts on top (#68)
    # Motivation
    
    In case of errors display the toasts always on the top including above modals.
    
    # Changes
    
    - split `z-index` for toasts in two, one for normal behavior one for errors

--- src/lib/components/Toasts.svelte
+++ src/lib/components/Toasts.svelte
@@ -2,11 +2,17 @@
   import { toastsStore } from "$lib/stores/toasts.store";
   import Toast from "./Toast.svelte";
   import { layoutBottomOffset } from "$lib/stores/layout.store";
+
+  let hasErrors: boolean;
+  $: hasErrors = $toastsStore?.find(({ level }) =>
+    ["error", "warn"].includes(level)
+  );
 </script>
 
 {#if $toastsStore.length > 0}
   <div
     class="wrapper"
+    class:error={hasErrors}
     style={`--layout-bottom-offset: ${$layoutBottomOffset}px`}
   >
     {#each $toastsStore as msg (msg.id)}
@@ -33,7 +39,11 @@
     flex-direction: column;
     gap: var(--padding);
 
-    z-index: var(--toast-z-index);
+    z-index: var(--toast-info-z-index);
+
+    &.error {
+      z-index: var(--toast-error-z-index);
+    }
 
     @include media.min-width(large) {
       // A little narrowwer than the section to differentiate notifications from content
