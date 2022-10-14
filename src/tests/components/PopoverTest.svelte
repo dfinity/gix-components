76446d8322b1ab390e0f5a3489706ae8e84b827f
Formatting Committer <nobody@example.com>
2022-10-14 07:51:22 +0000

    Updating formatting

--- src/tests/components/PopoverTest.svelte
+++ src/tests/components/PopoverTest.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-    import Popover from "$lib/components/Popover.svelte";
-  </script>
-  
-  <Popover visible={true}>
-    <div data-tid="Popover-slot" />
-  </Popover>
-  
+  import Popover from "$lib/components/Popover.svelte";
+</script>
+
+<Popover visible={true}>
+  <div data-tid="Popover-slot" />
+</Popover>
