2c1e6a99c2b6dd1b8051715c3bcee6a24fb0bbfc
David Dal Busco <david.dalbusco@dfinity.org>
2022-10-17 11:35:56 +0200

    feat: color is an enum

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,4 +4,4 @@ export * from "./icons";
 export type { ToastMsg, ToastLevel } from "./types/toast";
 export * from "./stores/toasts.store";
 
-export type { Color } from "./types/color";
+export { Color } from "./types/color";
