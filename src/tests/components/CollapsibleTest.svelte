4b9932f30a2c071de0c46e7caa45acc77d4db27a
Sue Ann Kim <kimsueann@gmail.com>
2022-10-18 16:58:59 -0700

    Collapsible Test included

---
+++ src/tests/components/CollapsibleTest.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Collapsible from "$lib/components/Collapsible.svelte";
+  
+    export let props = {};
+  </script>
+  
+  <Collapsible {...props} on:nnsToggle>
+    <h3 slot="header">Jack</h3>
+    Sparrow
+  </Collapsible>
+  
