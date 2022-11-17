a99241b506a6e81efbd318e07a1530cf20cd520d
David Dal Busco <david.dalbusco@dfinity.org>
2022-11-17 14:07:23 +0100

    fix: menu backdrop accessibility and z-index (#118)
    # Motivation
    
    While displaying a backdrop over the content when the menu is open works, the accessibility is not good because the related element remains in the DOM even if not presented. This was an original decision for performance reason but after thinking about it, it's cleaner to remove it when not presented.
    
    In addition, this PR also fixes a z-index issue.
    
    # Changes
    
    - add backdrop to dom over content only when needed
    - close "menu open store" on wide screen with JS (to solve above requirements)
    - fix z-index quirks

--- src/lib/components/SplitPane.svelte
+++ src/lib/components/SplitPane.svelte
@@ -1,3 +1,23 @@
+<script lang="ts">
+  import { layoutMenuOpen } from "../stores/layout.store";
+
+  let innerWidth = 0;
+
+  // Close menu if it was opened and the viewport width becomes larger than xlarge screen (where the menu becomes sticky)
+  const onWindowSizeChange = (innerWidth: number) => {
+    if (!$layoutMenuOpen) {
+      return;
+    }
+
+    // The media query breakpoint to stick the menu is media xlarge 1300px
+    layoutMenuOpen.set(innerWidth > 1300 ? false : $layoutMenuOpen);
+  };
+
+  $: onWindowSizeChange(innerWidth);
+</script>
+
+<svelte:window bind:innerWidth />
+
 <div class="split-pane">
   <slot name="menu" />
   <slot />
