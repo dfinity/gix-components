eb1287d38d79741db1c2b339ea6fd051c13ecdd8
David Dal Busco <david.dalbusco@dfinity.org>
2022-11-24 13:35:40 +0100

    feat: new icon north east

---
+++ src/lib/icons/IconNorthEast.svelte
@@ -0,0 +1,20 @@
+<!-- source: DFINITY foundation -->
+<script lang="ts">
+  import { DEFAULT_ICON_SIZE } from "$lib/constants/constants";
+  export let size = `${DEFAULT_ICON_SIZE}px`;
+</script>
+
+<svg
+  width={size}
+  height={size}
+  viewBox="0 0 20 20"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M4.03728 16.0002L2.97662 14.9395L12.9952 4.92111L4.99895 4.92094L4.99898 3.42094L14.805 3.42115L15.555 3.42117L15.5549 4.17116L15.5548 14.0004L14.0548 14.0004L14.0549 5.98272L4.03728 16.0002Z"
+    fill="currentColor"
+  />
+</svg>
