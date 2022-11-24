3f40cebd4ed0b5e33765c624971fabd886ec3d46
David Dal Busco <david.dalbusco@dfinity.org>
2022-11-24 18:45:04 +0100

    test: island

---
+++ src/tests/lib/components/Island.spec.ts
@@ -0,0 +1,14 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render } from "@testing-library/svelte";
+import IslandTest from "./IslandTest.svelte";
+
+describe("Island", () => {
+  it("should render slotted element", () => {
+    const { getByTestId } = render(IslandTest);
+
+    expect(getByTestId("island-test-slot")).not.toBeNull();
+  });
+});
