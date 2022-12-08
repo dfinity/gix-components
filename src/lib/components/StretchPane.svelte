45445ea24af224ba7d769fcf0f7a5092716d9a0e
David Dal Busco <david.dalbusco@dfinity.org>
2022-12-08 11:37:11 +0100

    feat: hide menu on large screen

--- src/lib/components/StretchPane.svelte
+++ src/lib/components/StretchPane.svelte
@@ -1,3 +1,23 @@
+<script lang="ts">
+    import { layoutMenuOpen } from "../stores/layout.store";
+
+    let innerWidth = 0;
+
+    // Close menu if it was opened and the viewport width becomes larger than large screen (where the menu becomes sticky)
+    const onWindowSizeChange = (innerWidth: number) => {
+        if (!$layoutMenuOpen) {
+            return;
+        }
+
+        // The media query breakpoint to stick the menu is media large 1024px
+        layoutMenuOpen.set(innerWidth > 1024 ? false : $layoutMenuOpen);
+    };
+
+    $: onWindowSizeChange(innerWidth);
+</script>
+
+<svelte:window bind:innerWidth />
+
 <div class="stretch-pane">
     <slot name="menu" />
     <slot />
@@ -10,6 +30,8 @@
   .stretch-pane {
     position: absolute;
     @include display.inset;
+
+    padding-top: var(--header-offset, 0px);
 
     display: flex;
     flex-flow: row nowrap;
