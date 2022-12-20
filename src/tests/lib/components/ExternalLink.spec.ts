c1866a9618a3fff630bb0ecbc2fc2666dad6793c
Formatting Committer <nobody@example.com>
2022-12-20 15:50:29 +0000

    Updating formatting

--- src/tests/lib/components/ExternalLink.spec.ts
+++ src/tests/lib/components/ExternalLink.spec.ts
@@ -2,41 +2,41 @@
  * @jest-environment jsdom
  */
 
-import {render} from "@testing-library/svelte";
 import ExternalLink from "$lib/components/ExternalLink.svelte";
+import { render } from "@testing-library/svelte";
 
 describe("ExternalLink", () => {
-    it("should render a link", () => {
-        const {container} = render(ExternalLink, {
-            props: {
-                href: "https://internetcomputer.org"
-            }
-        });
-        const a = container.querySelector("a");
-        expect(a).not.toBeNull();
+  it("should render a link", () => {
+    const { container } = render(ExternalLink, {
+      props: {
+        href: "https://internetcomputer.org",
+      },
     });
+    const a = container.querySelector("a");
+    expect(a).not.toBeNull();
+  });
 
-    it("should target external", () => {
-        const {container} = render(ExternalLink, {
-            props: {
-                href: "https://internetcomputer.org"
-            }
-        });
-        const a = container.querySelector("a") as unknown as HTMLLinkElement;
-        expect(a.getAttribute("rel")).toEqual("external noopener noreferrer");
-        expect(a.getAttribute("target")).toEqual("_blank");
+  it("should target external", () => {
+    const { container } = render(ExternalLink, {
+      props: {
+        href: "https://internetcomputer.org",
+      },
     });
+    const a = container.querySelector("a") as unknown as HTMLLinkElement;
+    expect(a.getAttribute("rel")).toEqual("external noopener noreferrer");
+    expect(a.getAttribute("target")).toEqual("_blank");
+  });
 
-    it("should best accessible", () => {
-        const alt = "Test test test";
+  it("should best accessible", () => {
+    const alt = "Test test test";
 
-        const {container} = render(ExternalLink, {
-            props: {
-                href: "https://internetcomputer.org",
-                alt
-            }
-        });
-        const a = container.querySelector("a") as unknown as HTMLLinkElement;
-        expect(a.getAttribute("alt")).toEqual(alt);
+    const { container } = render(ExternalLink, {
+      props: {
+        href: "https://internetcomputer.org",
+        alt,
+      },
     });
+    const a = container.querySelector("a") as unknown as HTMLLinkElement;
+    expect(a.getAttribute("alt")).toEqual(alt);
+  });
 });
