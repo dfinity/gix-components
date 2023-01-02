7adfa9f6cbfc32f2d83cb46dbc3ed8198fbb15f7
Formatting Committer <nobody@example.com>
2023-01-02 10:17:37 +0000

    Updating formatting

--- src/docs/components/DocsCardNav.svelte
+++ src/docs/components/DocsCardNav.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
-    import Card from "$lib/components/Card.svelte";
-    import {goto} from "$app/navigation";
-    import {page} from "$app/stores";
-    
-    export let url: string;
+  import Card from "$lib/components/Card.svelte";
+  import { goto } from "$app/navigation";
+  import { page } from "$app/stores";
 
-    let routeId = "";
-    $: routeId = $page.route.id ?? "";
+  export let url: string;
+
+  let routeId = "";
+  $: routeId = $page.route.id ?? "";
 </script>
 
-<Card role="link" on:click={() => goto(url)} selected={routeId.includes(url)} transparent={!routeId.includes(url)}>
-    <span class="title"><slot /></span>
+<Card
+  role="link"
+  on:click={() => goto(url)}
+  selected={routeId.includes(url)}
+  transparent={!routeId.includes(url)}
+>
+  <span class="title"><slot /></span>
 </Card>
