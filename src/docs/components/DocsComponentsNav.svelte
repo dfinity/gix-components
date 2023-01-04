dc26d3d271ab527aa4b447e8ea7e47b7fbd04152
David Dal Busco <david.dalbusco@dfinity.org>
2023-01-04 09:29:20 +0100

    feat: remove global selector

--- src/docs/components/DocsComponentsNav.svelte
+++ src/docs/components/DocsComponentsNav.svelte
@@ -3,68 +3,82 @@
   import Nav from "$lib/components/Nav.svelte";
 </script>
 
-<Nav>
-  <h3 slot="title">Pick a component</h3>
+<div>
+  <Nav>
+    <h3 slot="title">Pick a component</h3>
 
-  <DocsCardNav url="/components/bottom-sheet">Bottom Sheet</DocsCardNav>
+    <DocsCardNav url="/components/bottom-sheet">Bottom Sheet</DocsCardNav>
 
-  <DocsCardNav url="/components/busy-screen">BusyScreen</DocsCardNav>
+    <DocsCardNav url="/components/busy-screen">BusyScreen</DocsCardNav>
 
-  <DocsCardNav url="/components/card">Card</DocsCardNav>
+    <DocsCardNav url="/components/card">Card</DocsCardNav>
 
-  <DocsCardNav url="/components/checkbox">Checkbox</DocsCardNav>
+    <DocsCardNav url="/components/checkbox">Checkbox</DocsCardNav>
 
-  <DocsCardNav url="/components/collapsible">Collapsible</DocsCardNav>
+    <DocsCardNav url="/components/collapsible">Collapsible</DocsCardNav>
 
-  <DocsCardNav url="/components/content">Content</DocsCardNav>
+    <DocsCardNav url="/components/content">Content</DocsCardNav>
 
-  <DocsCardNav url="/components/copy">Copy</DocsCardNav>
+    <DocsCardNav url="/components/copy">Copy</DocsCardNav>
 
-  <DocsCardNav url="/components/dropdown">Dropdown</DocsCardNav>
+    <DocsCardNav url="/components/dropdown">Dropdown</DocsCardNav>
 
-  <DocsCardNav url="/components/external-link">External Link</DocsCardNav>
+    <DocsCardNav url="/components/external-link">External Link</DocsCardNav>
 
-  <DocsCardNav url="/components/header-title">Header Title</DocsCardNav>
+    <DocsCardNav url="/components/header-title">Header Title</DocsCardNav>
 
-  <DocsCardNav url="/components/html">Html</DocsCardNav>
+    <DocsCardNav url="/components/html">Html</DocsCardNav>
 
-  <DocsCardNav url="/components/infinite-scroll">Infinite Scroll</DocsCardNav>
+    <DocsCardNav url="/components/infinite-scroll">Infinite Scroll</DocsCardNav>
 
-  <DocsCardNav url="/components/input-range">Input Range</DocsCardNav>
+    <DocsCardNav url="/components/input-range">Input Range</DocsCardNav>
 
-  <DocsCardNav url="/components/island">Island</DocsCardNav>
+    <DocsCardNav url="/components/island">Island</DocsCardNav>
 
-  <DocsCardNav url="/components/key-value">Key Value</DocsCardNav>
+    <DocsCardNav url="/components/key-value">Key Value</DocsCardNav>
 
-  <DocsCardNav url="/components/key-value-info">Key Value Info</DocsCardNav>
+    <DocsCardNav url="/components/key-value-info">Key Value Info</DocsCardNav>
 
-  <DocsCardNav url="/components/layout">Layout</DocsCardNav>
+    <DocsCardNav url="/components/layout">Layout</DocsCardNav>
 
-  <DocsCardNav url="/components/modal">Modal</DocsCardNav>
+    <DocsCardNav url="/components/modal">Modal</DocsCardNav>
 
-  <DocsCardNav url="/components/popover">Popover</DocsCardNav>
+    <DocsCardNav url="/components/popover">Popover</DocsCardNav>
 
-  <DocsCardNav url="/components/progress-bar">Progress Bar</DocsCardNav>
+    <DocsCardNav url="/components/progress-bar">Progress Bar</DocsCardNav>
 
-  <DocsCardNav url="/components/skeleton-text">Skeleton Text</DocsCardNav>
+    <DocsCardNav url="/components/skeleton-text">Skeleton Text</DocsCardNav>
 
-  <DocsCardNav url="/components/spinner">Spinner</DocsCardNav>
+    <DocsCardNav url="/components/spinner">Spinner</DocsCardNav>
 
-  <DocsCardNav url="/components/tag">Tag</DocsCardNav>
+    <DocsCardNav url="/components/tag">Tag</DocsCardNav>
 
-  <DocsCardNav url="/components/toasts">Toasts</DocsCardNav>
+    <DocsCardNav url="/components/toasts">Toasts</DocsCardNav>
 
-  <DocsCardNav url="/components/theme-toggle">Theme Toggle</DocsCardNav>
+    <DocsCardNav url="/components/theme-toggle">Theme Toggle</DocsCardNav>
 
-  <DocsCardNav url="/components/toggle">Toggle</DocsCardNav>
+    <DocsCardNav url="/components/toggle">Toggle</DocsCardNav>
 
-  <DocsCardNav url="/components/value">Value</DocsCardNav>
+    <DocsCardNav url="/components/value">Value</DocsCardNav>
 
-  <DocsCardNav url="/components/wizard-modal">Wizard Modal</DocsCardNav>
-</Nav>
+    <DocsCardNav url="/components/wizard-modal">Wizard Modal</DocsCardNav>
+  </Nav>
+</div>
 
 <style lang="scss">
+  @use "../../lib/styles/mixins/media";
+
   h3 {
     padding: var(--padding-2x) 0 0;
+  }
+
+  div {
+    :global(article:not(.selected)) {
+      display: none;
+
+      @include media.min-width(large) {
+        display: block;
+      }
+    }
   }
 </style>
--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -13,7 +13,8 @@
     margin: 0;
 
     @include media.min-width(large) {
-      padding: var(--padding-4x) var(--padding-4x) calc(var(--padding-6x) + var(--padding-0_25x));
+      padding: var(--padding-4x) var(--padding-4x)
+        calc(var(--padding-4x) + var(--padding-0_5x));
     }
   }
 
@@ -22,19 +23,5 @@
 
     margin: 0;
     padding: 0 var(--padding-2x);
-  }
-
-  nav {
-    :global(article:first-of-type) {
-      margin-top: 0;
-    }
-
-    :global(article:not(.selected)) {
-      display: none;
-
-      @include media.min-width(large) {
-        display: block;
-      }
-    }
   }
 </style>
