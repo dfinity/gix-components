0fdf8d5d82ba7e000cbe534a3d81f4a2f39d7152
David Dal Busco <david.dalbusco@dfinity.org>
2023-01-09 08:09:23 +0100

    test: nav

---
+++ src/tests/lib/components/Nav.spec.ts
@@ -0,0 +1,20 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render } from "@testing-library/svelte";
+import NavTest from "./NavTest.svelte";
+
+describe("Nav", () => {
+  it("should render slotted elements", () => {
+    const { getByTestId } = render(NavTest);
+
+    expect(getByTestId("nav-test-1")).not.toBeNull();
+    expect(getByTestId("nav-test-2")).not.toBeNull();
+  });
+
+  it("should render a nav", () => {
+    const { container } = render(NavTest);
+    expect(container.querySelector("nav")).not.toBeNull();
+  });
+});
---
+++ src/tests/lib/components/NavTest.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Nav from "$lib/components/Nav.svelte";
+</script>
+
+<Nav>
+  <div data-tid="nav-test-1" />
+  <div data-tid="nav-test-2" />
+</Nav>
