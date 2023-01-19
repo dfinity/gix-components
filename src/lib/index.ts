23507c4ccb9098b911cedf81bfc98b5dbbc23be3
David Dal Busco <david.dalbusco@dfinity.org>
2023-01-19 12:59:33 +0100

    chore: format

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,6 +3,7 @@ export * from "./constants/constants";
 export * from "./icons";
 export * from "./stores/busy.store";
 export * from "./stores/layout.store";
+export * from "./stores/toasts.store";
 export * from "./stores/wizard.state";
 export type { BusyState } from "./types/busy";
 export type { ToastLevel, ToastMsg } from "./types/toast";
