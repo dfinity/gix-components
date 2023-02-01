3af672dafcc8d26af222d40dcd70a427637b8681
David Dal Busco <david.dalbusco@dfinity.org>
2023-02-01 17:13:52 +0100

    feat: expose constant for extra large breakpoint in JS

--- src/lib/constants/constants.ts
+++ src/lib/constants/constants.ts
@@ -2,3 +2,5 @@ export const DEFAULT_ICON_SIZE = 20;
 
 // 1024 is the CSS $breakpoint-large size
 export const BREAKPOINT_LARGE = 1024;
+// 1300 is the CSS $breakpoint-extra-large size
+export const BREAKPOINT_EXTRA_LARGE = 1300;
