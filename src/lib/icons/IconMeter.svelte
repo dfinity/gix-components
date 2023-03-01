5d2f62b837cd8bd2dea3053250a864eb89e5910c
David Dal Busco <david.dalbusco@dfinity.org>
2023-03-01 09:43:49 +0100

    feat: dynamic icon meter

--- src/lib/icons/IconMeter.svelte
+++ src/lib/icons/IconMeter.svelte
@@ -11,26 +11,20 @@
 >
   <path
     d="M6 26C6 14.9543 14.9543 6 26 6C37.0457 6 46 14.9543 46 26"
-    stroke="url(#paint0_linear_1253_421033)"
+    stroke="url(#paint0_linear_1253_421594)"
     stroke-width="5"
     stroke-linecap="round"
   />
   <path
+    class="progress"
     fill-rule="evenodd"
     clip-rule="evenodd"
     d="M24.2308 23.327L26 4.75L27.7692 23.327C28.5154 23.8727 29 24.7548 29 25.75C29 27.4069 27.6569 28.75 26 28.75C24.3431 28.75 23 27.4069 23 25.75C23 24.7548 23.4846 23.8727 24.2308 23.327Z"
     fill="currentColor"
   />
-  <path
-    fill-rule="evenodd"
-    clip-rule="evenodd"
-    d="M24.2308 23.327L26 4.75L27.7692 23.327C28.5154 23.8727 29 24.7548 29 25.75C29 27.4069 27.6569 28.75 26 28.75C24.3431 28.75 23 27.4069 23 25.75C23 24.7548 23.4846 23.8727 24.2308 23.327Z"
-    fill="url(#paint2_linear_1253_421033)"
-    fill-opacity="0.1"
-  />
   <defs>
     <linearGradient
-      id="paint0_linear_1253_421033"
+      id="paint0_linear_1253_421594"
       x1="50.1026"
       y1="23.4359"
       x2="2.41026"
@@ -40,22 +34,16 @@
       <stop stop-color="#B767E5" />
       <stop offset="1" stop-color="#2A2046" />
     </linearGradient>
-    <linearGradient
-      id="paint2_linear_1253_421033"
-      x1="29"
-      y1="28.75"
-      x2="25.4504"
-      y2="28.6711"
-      gradientUnits="userSpaceOnUse"
-    >
-      <stop stop-color="white" stop-opacity="0.5" />
-      <stop offset="1" stop-opacity="0" />
-    </linearGradient>
   </defs>
 </svg>
 
 <style lang="scss">
   svg {
     color: var(--value-color);
+  }
+
+  .progress {
+    transform: rotate(var(--meter-rotate, 0deg));
+    transform-origin: 26px 26px;
   }
 </style>
