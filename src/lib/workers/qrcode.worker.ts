51ee42144b0d79871fb8a28e0443aae37a13ad77
David Dal Busco <david.dalbusco@dfinity.org>
2023-03-14 11:57:13 +0100

    feat: memory

--- src/lib/workers/qrcode.worker.ts
+++ src/lib/workers/qrcode.worker.ts
@@ -4,12 +4,31 @@ import type {
 } from "$lib/types/post-message";
 import jsQR from "jsqr";
 
+let lastScanTimestamp = performance.now();
+
+const maxScansPerSecond = 25;
+const minimumTimeBetweenScans = 1000 / maxScansPerSecond;
+
+/**
+ * Debounce to avoid jsQR memory overflow
+ */
+const debounceDecodeQRCode = (data: PostMessageDataRequest) => {
+  const timeSinceLastScan = performance.now() - lastScanTimestamp;
+
+  if (timeSinceLastScan < minimumTimeBetweenScans) {
+    return;
+  }
+
+  decodeQRCode(data);
+  lastScanTimestamp = performance.now();
+};
+
 onmessage = ({ data }: MessageEvent<PostMessage<PostMessageDataRequest>>) => {
   const { msg, data: imageData } = data;
 
   switch (msg) {
     case "nnsQRCodeDecode":
-      decodeQRCode(imageData);
+      debounceDecodeQRCode(imageData);
       return;
   }
 };
@@ -19,7 +38,9 @@ const decodeQRCode = ({
   width,
   height,
 }: PostMessageDataRequest) => {
-  const result = jsQR(data, width, height);
+  const result = jsQR(data, width, height, {
+    inversionAttempts: "dontInvert",
+  });
 
   if (result === undefined || result == null) {
     return;
