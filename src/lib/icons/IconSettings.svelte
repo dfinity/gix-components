15e563ae05dd0024403ca9a5c381c280fdd716c2
David Dal Busco <david.dalbusco@dfinity.org>
2023-03-20 08:51:41 +0100

    feat: icon settings (#174)
    # Motivation
    
    New icon "Settings"

---
+++ src/lib/icons/IconSettings.svelte
@@ -0,0 +1,17 @@
+<!-- source: https://fonts.google.com/icons?selected=Material%20Icons%20Outlined%3Atune%3A -->
+<script lang="ts">
+  import { DEFAULT_ICON_SIZE } from "$lib/constants/constants";
+
+  export let size = `${DEFAULT_ICON_SIZE}px`;
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  height={size}
+  width={size}
+  viewBox="0 0 48 48"
+  fill="currentColor"
+  ><path
+    d="M21.35 42V30.75h3v4.15H42v3H24.35V42ZM6 37.9v-3h12.35v3Zm9.35-8.3v-4.1H6v-3h9.35v-4.2h3v11.3Zm6-4.1v-3H42v3Zm8.3-8.25V6h3v4.1H42v3h-9.35v4.15ZM6 13.1v-3h20.65v3Z"
+  /></svg
+>
