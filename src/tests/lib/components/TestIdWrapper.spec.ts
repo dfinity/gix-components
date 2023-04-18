2cb90cc0950a0090cd9015272a29e5ede1ea6484
Llorenç <llorenc.muntaner@dfinity.org>
2023-04-18 11:07:48 +0200

    Feat: Fix formatting

--- src/tests/lib/components/TestIdWrapper.spec.ts
+++ src/tests/lib/components/TestIdWrapper.spec.ts
@@ -2,15 +2,15 @@
  * @jest-environment jsdom
  */
 
-import TestIdWrapper from "./TestIdWrapperTest.svelte";
 import { render } from "@testing-library/svelte";
+import TestIdWrapper from "./TestIdWrapperTest.svelte";
 
 describe("TestIdWrapper", () => {
-  const testId = "test-id"
-  const childTestId = "child-test-id"
+  const testId = "test-id";
+  const childTestId = "child-test-id";
   it("should wrap component with the passed test id", () => {
     const { queryByTestId } = render(TestIdWrapper, {
-      props: { testId }
+      props: { testId },
     });
 
     expect(queryByTestId(testId)).toBeInTheDocument();
@@ -18,7 +18,7 @@ describe("TestIdWrapper", () => {
 
   it("should render child component", () => {
     const { queryByTestId } = render(TestIdWrapper, {
-      props: { testId, childTestId }
+      props: { testId, childTestId },
     });
 
     expect(queryByTestId(childTestId)).toBeInTheDocument();
--- src/tests/lib/components/TestIdWrapperTest.svelte
+++ src/tests/lib/components/TestIdWrapperTest.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import Back from "$lib/components/Back.svelte";
-import TestIdWrapper from "$lib/components/TestIdWrapper.svelte";
+  import TestIdWrapper from "$lib/components/TestIdWrapper.svelte";
 
   export let testId: string | undefined = undefined;
   export let childTestId: string | undefined = undefined;
