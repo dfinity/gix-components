b1fc13f84cd1c0be10ec0bcceb768f433cea7e80
David Dal Busco <david.dalbusco@dfinity.org>
2023-05-03 14:54:55 +0200

    feat: new icons

---
+++ src/lib/icons/IconCheck.svelte
@@ -0,0 +1,19 @@
+<!-- source: DFINITY foundation -->
+<script lang="ts">
+  export let size = `24px`;
+</script>
+
+<svg
+  width={size}
+  height={size}
+  viewBox="0 0 32 32"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d="M6.5 17.282L13.3478 23.5L27.5 6.5"
+    stroke="currentColor"
+    stroke-width="4"
+    stroke-linecap="round"
+  />
+</svg>
