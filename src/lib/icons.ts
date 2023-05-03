355e28c754ec64c6c11cf4984ea355e51543b602
David Dal Busco <david.dalbusco@dfinity.org>
2023-05-03 15:07:28 +0200

    feat: button card style and icons (#209)

--- src/lib/icons.ts
+++ src/lib/icons.ts
@@ -2,7 +2,9 @@ export { default as IconAccountBalance } from "./icons/IconAccountBalance.svelte
 export { default as IconAdd } from "./icons/IconAdd.svelte";
 export { default as IconArrowRight } from "./icons/IconArrowRight.svelte";
 export { default as IconBack } from "./icons/IconBack.svelte";
+export { default as IconCheck } from "./icons/IconCheck.svelte";
 export { default as IconCheckCircle } from "./icons/IconCheckCircle.svelte";
+export { default as IconClock } from "./icons/IconClock.svelte";
 export { default as IconClose } from "./icons/IconClose.svelte";
 export { default as IconCopy } from "./icons/IconCopy.svelte";
 export { default as IconDarkMode } from "./icons/IconDarkMode.svelte";
