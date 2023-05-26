1bb3cf3e3fe118707af4280af9650b95effe9ccd
David de Kloet <122978264+dskloetd@users.noreply.github.com>
2023-05-26 15:16:30 +0200

    Add testId to Value component (#218)

--- src/lib/components/Value.svelte
+++ src/lib/components/Value.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   export let ariaLabel: string | undefined = undefined;
+  export let testId: string | undefined = undefined;
 </script>
 
-<span class="value" aria-label={ariaLabel}>
+<span class="value" aria-label={ariaLabel} data-tid={testId}>
   <slot />
 </span>
