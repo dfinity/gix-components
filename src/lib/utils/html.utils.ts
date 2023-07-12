5c01479bae15e55b565051d26c543d6c76a11302
David Dal Busco <david.dalbusco@dfinity.org>
2023-07-12 15:53:16 +0200

    docs: jest

--- src/lib/utils/html.utils.ts
+++ src/lib/utils/html.utils.ts
@@ -42,7 +42,7 @@ const flagTargetAttributeHook = (node: Element) => {
 /**
  * Sanitize a text with DOMPurify.
  *
- * Note: this library needs a workaround to work in the NodeJS context - i.e. for our jest test suite.
+ * Note: this library needs a workaround to work in the NodeJS context - i.e. for our vitest test suite.
  * See the vitest.setup.ts for details.
  */
 export const sanitize = (text: string): string => {
@@ -52,7 +52,7 @@ export const sanitize = (text: string): string => {
       if (typeof DOMPurify.sanitize === "function") {
         domPurify = DOMPurify;
       } else if (typeof global.DOMPurify.sanitize === "function") {
-        // utilize jest version
+        // utilize NodeJS version
         domPurify = global.DOMPurify;
       }
 
