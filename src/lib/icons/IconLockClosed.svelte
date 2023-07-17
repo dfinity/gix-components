bfda9b2f1514fb0f3ed6f178ad87ec900f6c98b1
Llorenç <llorenc.muntaner@dfinity.org>
2023-07-17 09:09:52 +0200

    Feat: Rename and fix light theme

--- src/lib/icons/IconLockClosed.svelte
+++ src/lib/icons/IconLockClosed.svelte
@@ -10,17 +10,16 @@
   height={size}
   viewBox="0 0 20 20"
   fill="none"
+  stroke="currentColor"
   xmlns="http://www.w3.org/2000/svg"
 >
   <path
     d="M2.75 9C2.75 7.75736 3.75736 6.75 5 6.75H15C16.2426 6.75 17.25 7.75736 17.25 9V15C17.25 16.2426 16.2426 17.25 15 17.25H5C3.75736 17.25 2.75 16.2426 2.75 15V9Z"
-    stroke="#D9D3E5"
     stroke-width="1.5"
   />
   <path
     d="M13 7V3C13 1.89543 12.1046 1 11 1H9C7.89543 1 7 1.89543 7 3V7"
-    stroke="#D9D3E5"
     stroke-width="1.5"
   />
-  <circle cx="10" cy="12" r="1" fill="#D9D3E5" />
+  <circle cx="10" cy="12" r="1" fill="currentColor" />
 </svg>
--- src/lib/icons/IconPace.svelte
+++ src/lib/icons/IconPace.svelte
@@ -10,18 +10,17 @@
   height={size}
   viewBox="0 0 20 20"
   fill="none"
+  stroke="currentColor"
   xmlns="http://www.w3.org/2000/svg"
 >
   <path
     d="M18 10C18 5.58172 14.4183 2 10 2C5.58172 2 2 5.58172 2 10C2 14.4183 5.58172 18 10 18"
-    stroke="#D9D3E5"
     stroke-width="1.5"
     stroke-linecap="round"
     stroke-linejoin="round"
   />
   <path
     d="M18 10C18 14.4183 14.4183 18 10 18"
-    stroke="#D9D3E5"
     stroke-width="1.5"
     stroke-linecap="round"
     stroke-linejoin="round"
@@ -29,7 +28,6 @@
   />
   <path
     d="M10 5V10.25L13 12"
-    stroke="#D9D3E5"
     stroke-width="1.5"
     stroke-linecap="round"
     stroke-linejoin="round"
