52b8bb5630092eca0ceaa16d70f0620e32a13aac
David Dal Busco <david.dalbusco@dfinity.org>
2023-07-31 14:36:37 +0200

    chore: revert size

--- src/lib/constants/constants.ts
+++ src/lib/constants/constants.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_ICON_SIZE = 20;
 
-// 992 is the CSS $breakpoint-large size
-export const BREAKPOINT_LARGE = 992;
-// 1200 is the CSS $breakpoint-extra-large size
-export const BREAKPOINT_EXTRA_LARGE = 1200;
+// 1024 is the CSS $breakpoint-large size
+export const BREAKPOINT_LARGE = 1024;
+// 1300 is the CSS $breakpoint-extra-large size
+export const BREAKPOINT_EXTRA_LARGE = 1300;
