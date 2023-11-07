3741b7d2b311fcb020dcf84aee3eca0efef0a38e
David de Kloet <david.de.kloet@dfinity.org>
2023-11-07 17:16:41 +0100

    Add 3 icons: Up, Down, ErrorOutline

---
+++ src/lib/icons/IconUp.svelte
@@ -0,0 +1,20 @@
+<!-- source: DFINITY foundation -->
+<script lang="ts">
+  import { DEFAULT_ICON_SIZE } from "$lib/constants/constants";
+  export let size = `${DEFAULT_ICON_SIZE}px`;
+</script>
+
+<svg
+  height={size}
+  width={size}
+  viewBox="0 0 20 20"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    fill="currentColor"
+    d="M10.7604 17.0756H9.35472L9.35459 2.94363L4.09146 8.56005L2.99693 7.53437L9.51014 0.583979L10.0574 0L10.6046 0.58396L17.1027 7.51769L16.0082 8.5434L10.7603 2.94362L10.7604 17.0756ZM17.9998 18V19.5L2 19.4998V17.9998L17.9998 18Z"
+  />
+</svg>
