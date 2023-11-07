e8393543795c44367e31e072c3c42882b3856605
mstrasinskis <maksims.strasinskis@dfinity.org>
2023-11-07 09:30:50 +0100

    Add collapse all button

---
+++ src/lib/icons/IconCollapseAll.svelte
@@ -0,0 +1,27 @@
+<!-- source: DFINITY foundation -->
+<script lang="ts">
+  import { DEFAULT_ICON_SIZE } from "$lib/constants/constants";
+</script>
+
+<svg
+  width={DEFAULT_ICON_SIZE}
+  height={DEFAULT_ICON_SIZE}
+  viewBox="0 0 20 20"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d="m 15.330508,2.9364407 c -1.75,1.75 -3.5,3.5 -5.25,5.25 -1.7499998,-1.75 -3.4999997,-3.5 -5.2499995,-5.25"
+    stroke="currentColor"
+    stroke-width="1.5"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="m 4.7838983,16.898274 c 1.7499999,-1.75 3.4999998,-3.5 5.2499997,-5.25 1.75,1.75 3.500001,3.5 5.250001,5.25"
+    stroke="currentColor"
+    stroke-width="1.5"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+</svg>
