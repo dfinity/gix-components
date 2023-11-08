0888564328638f826c37bba4bda893d6b1422f07
Llorenç <llorenc.muntaner@dfinity.org>
2023-11-08 09:08:08 +0100

    Chore: Export breakpoint constants

--- src/lib/constants/constants.ts
+++ src/lib/constants/constants.ts
@@ -1,5 +1,9 @@
 export const DEFAULT_ICON_SIZE = 20;
 
+// 576 is the CSS $breakpoint-small size
+export const BREAKPOINT_SMALL = 576;
+// 768 is the CSS $breakpoint-medium size
+export const BREAKPOINT_MEDIUM = 768;
 // 1024 is the CSS $breakpoint-large size
 export const BREAKPOINT_LARGE = 1024;
 // 1300 is the CSS $breakpoint-extra-large size
