16d12e55c4d5bbca57a7cdd6521ae26e67c4587e
David de Kloet <david.de.kloet@dfinity.org>
2023-12-07 11:35:26 +0100

    Add IconReimbursed

---
+++ src/lib/icons/IconReimbursed.svelte
@@ -0,0 +1,20 @@
+<!-- source: DFINITY foundation -->
+<script lang="ts">
+  import { DEFAULT_ICON_SIZE } from "$lib/constants/constants";
+  export let size = `${DEFAULT_ICON_SIZE}px`;
+</script>
+
+<svg
+  width="{size}"
+  height="{size}"
+  viewBox="0 0 20 20"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="1.5"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path d="M16.9793 11.1559V9.9918C16.9793 6.13516 13.8449 3.00782 9.9793 3.00782C8.9277 3.00652 7.88936 3.24244 6.94157 3.69801C5.99377 4.15358 5.16094 4.81706 4.50508 5.63907M3 8.84219V10.0063C3 13.8672 6.13281 16.9922 10 16.9922C11.0486 16.9907 12.0836 16.7552 13.0297 16.303C13.9758 15.8508 14.809 15.1932 15.4688 14.3781"/>
+  <path d="M1.25 10L2.96875 8.28125L4.76562 10M18.75 10L17.0312 11.7187L15.2344 10"/>
+</svg>
