d5280c67de0dc9686daeb766e08cfaf333816d46
David de Kloet <david.de.kloet@dfinity.org>
2023-12-07 11:45:59 +0100

    format

--- src/lib/icons.ts
+++ src/lib/icons.ts
@@ -41,6 +41,7 @@ export { default as IconNorthEast } from "./icons/IconNorthEast.svelte";
 export { default as IconPace } from "./icons/IconPace.svelte";
 export { default as IconPassword } from "./icons/IconPassword.svelte";
 export { default as IconQRCodeScanner } from "./icons/IconQRCodeScanner.svelte";
+export { default as IconReimbursed } from "./icons/IconReimbursed.svelte";
 export { default as IconRight } from "./icons/IconRight.svelte";
 export { default as IconRocketLaunch } from "./icons/IconRocketLaunch.svelte";
 export { default as IconSettings } from "./icons/IconSettings.svelte";
