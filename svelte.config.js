a87f78540e526a4f0be4785ee7533657715a2910
David Dal Busco <david.dalbusco@dfinity.org>
2023-12-18 07:59:40 +0100

    build: SvelteKit v2

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,13 @@
 import adapter from "@sveltejs/adapter-static";
-import autoprefixer from "autoprefixer";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { mdsvex } from "mdsvex";
-import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessor
   preprocess: [
-    preprocess({
-      postcss: {
-        plugins: [autoprefixer],
-      },
-    }),
+    vitePreprocess(),
     mdsvex({
       extensions: [".md"],
     }),
