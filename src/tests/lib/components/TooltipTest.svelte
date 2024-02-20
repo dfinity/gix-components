367d349ed23f53f4fd8a5eff4a6f5e9e12229c1e
David de Kloet <122978264+dskloetd@users.noreply.github.com>
2024-02-20 08:37:24 +0100

    Hide tooltip when there is no text (#386)
    # Motivation
    
    Sometimes we want to have a tooltip conditional on some logic, such as
    [here](https://github.com/dfinity/nns-dapp/blob/9f0bd72c6e315adf7443bed1ee14571e600cd2a7/frontend/src/lib/components/neuron-detail/NeuronSelectPercentage.svelte#L65).
    It would be convenient if the tooltip was just hidden if there is no
    text.
    
    # Changes
    
    1. Allow `text` to be undefined and hide the tooltip in that case.
    2. Reposition the tooltip if the `text` changes.
    3. Change the showcase to allow changing the `text`.
    4. Add unit tests.
    
    # Screenshots
    
    Looks the same when it's not hidden.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/tests/lib/components/TooltipTest.svelte
+++ src/tests/lib/components/TooltipTest.svelte
@@ -1,7 +1,9 @@
-<script>
+<script lang="ts">
   import Tooltip from "$lib/components/Tooltip.svelte";
+
+  export let text: string | undefined = undefined;
 </script>
 
-<Tooltip id="tid" text="text">
+<Tooltip id="tid" {text}>
   <p>content</p>
 </Tooltip>
