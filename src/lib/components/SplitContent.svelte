bbfda1357f6b03c867b0c62c5abf816356f3cf2a
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-03-27 13:14:42 +0100

    feat: perceivedOnMobile prop in SplitContent

--- src/lib/components/SplitContent.svelte
+++ src/lib/components/SplitContent.svelte
@@ -7,20 +7,33 @@
   import Header from "$lib/components/Header.svelte";
   import ContentBackdrop from "$lib/components/ContentBackdrop.svelte";
 
+  const BREAKPOINT_LARGE = 1024;
+
   export let back = false;
+  export let perceivedOnMobile = true;
+
+  let innerWidth = 0;
+  let isBreakpointLarge = false;
+  $: isBreakpointLarge = innerWidth > BREAKPOINT_LARGE;
+
+  let isPerceived = true;
+  $: isPerceived = perceivedOnMobile || isBreakpointLarge;
 
   // Same as in <Content />
   onDestroy(() => ($layoutBottomOffset = 0));
 </script>
 
+<svelte:window bind:innerWidth />
 <div
   class="content"
+  class:isPerceived
   style={`--layout-bottom-offset: ${$layoutBottomOffset}px; --content-overflow-y: ${$layoutContentScrollY}`}
 >
   <div class="start">
     <div class="scrollable-content-start">
-      <ContentBackdrop />
-
+      {#if isPerceived}
+        <ContentBackdrop />
+      {/if}
       <slot name="start" />
     </div>
   </div>
@@ -53,6 +66,10 @@
     @include media.min-width(large) {
       flex-flow: row nowrap;
     }
+
+    &:not(.isPerceived) {
+      --content-start-height: calc(75px + var(--padding-2x));
+    }
   }
 
   .start,
@@ -62,10 +79,11 @@
     box-sizing: border-box;
   }
 
+  .content.isPerceived .start {
+    background: var(--content-start-background);
+  }
   .start {
     min-height: var(--content-start-height);
-
-    background: var(--content-start-background);
     color: var(--content-start-color);
 
     @include media.min-width(large) {
