bfe5e1bef516316c5ee4d54bde79d8687ee750a9
David de Kloet <david.de.kloet@dfinity.org>
2024-06-17 17:29:43 +0200

    Support rich content in tooltip

--- src/tests/lib/components/TooltipTest.svelte
+++ src/tests/lib/components/TooltipTest.svelte
@@ -4,8 +4,18 @@
   export let id: string | undefined = undefined;
   export let idPrefix: string | undefined = undefined;
   export let text: string | undefined = undefined;
+  export let slotText: string | undefined = undefined;
 </script>
 
-<Tooltip {id} {idPrefix} {text}>
-  <p>content</p>
-</Tooltip>
+{#if slotText === undefined}
+  <Tooltip {id} {idPrefix} {text}>
+    <p>content</p>
+  </Tooltip>
+{:else}
+  <Tooltip {id} {idPrefix}>
+    <p>content</p>
+    <div slot="tooltip-content">
+      {slotText}
+    </div>
+  </Tooltip>
+{/if}
