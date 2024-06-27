c5796d375ce0e1debef918af6f6920be2b7d6f29
David de Kloet <david.de.kloet@dfinity.org>
2024-06-27 09:11:39 +0200

    Add IconSort

---
+++ src/lib/icons/IconSort.svelte
@@ -0,0 +1,25 @@
+<!-- source: DFINITY foundation -->
+<script lang="ts">
+  import { DEFAULT_ICON_SIZE } from "$lib/constants/constants";
+
+  export let size = `${DEFAULT_ICON_SIZE}px`;
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width={size}
+  height={size}
+  viewBox="0 0 20 20"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="1.5"
+  stroke-linecap="round"
+>
+  <path d="M3.33301 14.166H9.33301" />
+  <path d="M3.3335 10L9.3335 10" />
+  <path d="M3.33301 5.83398L9.33301 5.83398" />
+  <path
+    d="M14.167 3.66602L14.167 16.666M14.167 3.66602L11.667 6.91602M14.167 3.66602L16.667 6.91602M14.167 16.666L16.667 13.416M14.167 16.666L11.667 13.416"
+    stroke-linejoin="round"
+  />
+</svg>
