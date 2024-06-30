6d6260869c46cf8d36c4bccfcfa035ae9f38c7b5
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-06-30 09:17:45 +0200

    New icon plus

---
+++ src/lib/icons/IconPlus.svelte
@@ -0,0 +1,26 @@
+<!-- source: DFINITY foundation -->
+<script lang="ts">
+  import { DEFAULT_ICON_SIZE } from "$lib/constants/constants";
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  height={DEFAULT_ICON_SIZE}
+  viewBox="0 0 20 20"
+  width={DEFAULT_ICON_SIZE}
+  fill="currentColor"
+>
+  <g clip-path="url(#a)">
+    <circle cx="10" cy="10" r="10" />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M9.17578 10.6794v3.18h1.50002v-3.18h3.1836v-1.5h-3.1836V6H9.17578v3.1794H6v1.5h3.17578Z"
+    />
+  </g>
+  <defs>
+    <clipPath id="a">
+      <path d="M0 0h20v20H0z" />
+    </clipPath>
+  </defs>
+</svg>
