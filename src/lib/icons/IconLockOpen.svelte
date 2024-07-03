a6814c1e088493f51e2b4186c026438d976dcf4f
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-07-03 18:36:04 +0200

    Update open lock icon

--- src/lib/icons/IconLockOpen.svelte
+++ src/lib/icons/IconLockOpen.svelte
@@ -6,39 +6,40 @@
 </script>
 
 <svg
-  width={size}
-  height={size}
-  viewBox="0 0 20 20"
-  fill="none"
-  xmlns="http://www.w3.org/2000/svg"
+        width={size}
+        height={size}
+        viewBox="0 0 21 21"
+        fill="none"
+        stroke="currentColor"
+        xmlns="http://www.w3.org/2000/svg"
 >
   <g clip-path="url(#clip0_32_69)">
     <path
-      d="M5.83151 8.00232H14.1623C15.1282 8.00232 15.9115 8.78561 15.9115 9.75232V15.5857C15.9115 16.5524 15.1282 17.3357 14.1623 17.3357H5.83151C4.86565 17.3357 4.08228 16.5524 4.08228 15.5857V9.75232C4.08228 8.78561 4.86565 8.00232 5.83151 8.00232Z"
-      stroke="currentColor"
-      stroke-width="1.5"
+            d="M6.70651 8.00232H15.0373C16.0032 8.00232 16.7865 8.78561 16.7865 9.75232V15.5857C16.7865 16.5524 16.0032 17.3357 15.0373 17.3357H6.70651C5.74065 17.3357 4.95728 16.5524 4.95728 15.5857V9.75232C4.95728 8.78561 5.74065 8.00232 6.70651 8.00232Z"
+            stroke="currentColor"
+            stroke-width="1.5"
     />
     <path
-      d="M6.66455 7.25232V5.58463C6.66455 3.74425 8.15648 2.25232 9.99687 2.25232H10.4125C12.0233 2.25232 13.3292 3.55816 13.3292 5.16899V5.16899"
-      stroke="currentColor"
-      stroke-width="1.5"
-      stroke-linecap="round"
+            d="M13.6492 7.25232V4.83899C13.6492 3.39585 14.8191 2.22595 16.2622 2.22595V2.22595C17.7053 2.22595 18.8752 3.39585 18.8752 4.83899V5.09517"
+            stroke="currentColor"
+            stroke-width="1.5"
+            stroke-linecap="round"
     />
     <path
-      d="M9.99487 12.6691H9.99988"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+            d="M10.8699 12.6691H10.8749"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
     />
   </g>
   <defs>
     <clipPath id="clip0_32_69">
       <rect
-        width="20"
-        height="20"
-        fill="currentColor"
-        transform="translate(0 0.585693)"
+              width="20"
+              height="20"
+              fill="currentColor"
+              transform="translate(0.875 0.585693)"
       />
     </clipPath>
   </defs>
