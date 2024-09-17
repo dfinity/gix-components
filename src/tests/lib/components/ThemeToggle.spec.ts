3e878b03c8b03fd1f7b753d074d80fcfbb4ef6b6
“Cosku <cosku.cinkilic@dfinity.org>
2024-09-17 14:11:37 +0200

    update theme toggle and tests

--- src/tests/lib/components/ThemeToggle.spec.ts
+++ src/tests/lib/components/ThemeToggle.spec.ts
@@ -1,4 +1,6 @@
 import ThemeToggle from "$lib/components/ThemeToggle.svelte";
+import IconDarkMode from "$lib/icons/IconDarkMode.svelte";
+import IconLightMode from "$lib/icons/IconLightMode.svelte";
 import { themeStore } from "$lib/stores/theme.store";
 import { Theme } from "$lib/types/theme";
 import { fireEvent, render } from "@testing-library/svelte";
@@ -6,30 +8,51 @@ import { get } from "svelte/store";
 import en from "../mocks/i18n.mock";
 
 describe("ThemeToggle", () => {
-  it("should render a toggle", () => {
+  it("should render a toggle button", () => {
     const { container } = render(ThemeToggle);
 
-    const input = container.querySelector("input") as HTMLInputElement;
+    const input = container.querySelector("button") as HTMLButtonElement;
     expect(input).not.toBeNull();
-    expect(input.getAttribute("type")).toEqual("checkbox");
   });
 
-  it("should render an accessible toggle", () => {
+  it("should render an accessible toggle button", () => {
     const { container } = render(ThemeToggle);
 
-    const input = container.querySelector("input") as HTMLInputElement;
+    const input = container.querySelector("button") as HTMLButtonElement;
     expect(input.getAttribute("aria-label")).toEqual(en.theme.switch_theme);
   });
 
   it("should switch theme", () => {
     const { container } = render(ThemeToggle);
 
-    const input = container.querySelector("input") as HTMLInputElement;
+    const input = container.querySelector("button") as HTMLButtonElement;
 
     fireEvent.click(input);
     expect(get(themeStore)).toEqual(Theme.LIGHT);
 
     fireEvent.click(input);
     expect(get(themeStore)).toEqual(Theme.DARK);
+  });
+
+  it("should render IconLightMode when theme is dark", () => {
+    themeStore.select(Theme.DARK);
+    const { container } = render(ThemeToggle);
+
+    const lightModeIcon = container.querySelector("svg");
+    expect(lightModeIcon).toBeInstanceOf(SVGSVGElement);
+    expect(container.innerHTML).toContain(
+      render(IconLightMode).container.innerHTML,
+    );
+  });
+
+  it("should render IconDarkMode when theme is light", () => {
+    themeStore.select(Theme.LIGHT);
+    const { container } = render(ThemeToggle);
+
+    const darkModeIcon = container.querySelector("svg");
+    expect(darkModeIcon).toBeInstanceOf(SVGSVGElement);
+    expect(container.innerHTML).toContain(
+      render(IconDarkMode).container.innerHTML,
+    );
   });
 });
