71ecfa9ce6959901fad85700f1747d359a687d11
Cosku Cinkilic <cosku.cinkilic@dfinity.org>
2024-10-05 01:00:12 +0200

    update menu and scss

--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-  import MenuBackground from "./MenuBackground.svelte";
   import { layoutMenuOpen } from "$lib/stores/layout.store";
   import { handleKeyPress } from "$lib/utils/keyboard.utils";
   import IconBack from "$lib/icons/IconBack.svelte";
   import { menuCollapsed, menuStore } from "$lib/stores/menu.store";
   import { i18n } from "$lib/stores/i18n";
+  import { themeStore } from "$lib/stores/theme.store";
+  import { nonNullish } from "@dfinity/utils";
+  import { fade } from "svelte/transition";
 
   export let sticky = true;
 
@@ -12,23 +14,30 @@
 </script>
 
 <div role="menu" class:open={$layoutMenuOpen}>
-  <MenuBackground>
-    <slot name="logo" slot="logo" />
-    <slot name="oneliner" slot="oneliner" />
-  </MenuBackground>
-
-  <div
-    class="inner"
-    class:sticky
-    data-tid="menu-inner"
-    class:open={$layoutMenuOpen}
-    role="button"
-    tabindex="-1"
-    on:click={close}
-    on:keypress={($event) => handleKeyPress({ $event, callback: close })}
-  >
-    <slot />
+  <div class="logo-nns logo">
+    <slot name="logo" />
   </div>
+
+  <div class="inner-wrapper">
+    <div
+      class="inner"
+      class:sticky
+      data-tid="menu-inner"
+      role="button"
+      tabindex="-1"
+      class:menu-collapsed={$menuCollapsed}
+      on:click={close}
+      on:keypress={($event) => handleKeyPress({ $event, callback: close })}
+    >
+      <slot />
+    </div>
+  </div>
+
+  {#if nonNullish($themeStore)}
+    <div in:fade class="bottom-logo logo">
+      <slot name="oneliner" />
+    </div>
+  {/if}
 
   <button
     class="menu-collapse icon-only"
@@ -46,83 +55,78 @@
   div[role="menu"] {
     @include interaction.initial;
 
-    box-sizing: border-box;
-
-    z-index: var(--menu-z-index);
-
-    --menu-logo-height: 65px;
-    --menu-logo-height-large: 88px;
-    --menu-bottom-logo-height: 68px;
-    --menu-stack: 1em;
-
-    padding-top: calc(
-      var(--menu-logo-height) + var(--padding-4x) + var(--header-offset, 0px)
-    );
-
-    padding-bottom: var(--menu-bottom-logo-height);
+    padding-top: calc(var(--padding-4x) + var(--header-offset, 0px));
 
     position: relative;
 
-    // Shift the menu on large screen e.g. if a banner is displayed
     @include media.min-width(large) {
-      padding: calc(
-          var(--menu-logo-height-large) + var(--padding-3x) +
-            var(--header-offset, 0px) - var(--menu-selection-outer-radius)
-        )
-        var(--padding-2x) var(--menu-bottom-logo-height);
-      // remove extra space because of menu selection touches the edge
-      padding-right: 0;
+      padding-top: calc(
+        var(--padding-3x) + var(--header-offset, 0px) - var(
+            --menu-selection-outer-radius
+          )
+      );
+    }
+  }
+
+  .inner-wrapper {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    overflow: hidden;
+
+    //space for scroll
+    padding-left: var(--padding-2x);
+    &.menu-collapsed {
+      //small screen collapse doesn't need a padding
+      padding-left: 0;
     }
   }
 
   .inner {
     display: flex;
     flex-direction: column;
+    flex-grow: 1;
     gap: var(--padding-0_5x);
 
     // More space for menu selection touches the edge;
     // otherwise the first selected menu entry would be cut off in mobile view.
     padding-top: var(--menu-selection-outer-radius);
 
-    width: 0;
     max-width: 100vw;
-    height: 100%;
+    flex-grow: 1;
 
     overflow-y: auto;
-    margin-left: -100%;
-
-    transition:
-      margin-left var(--animation-time-normal)
-        var(--menu-animation-timing-function),
-      width var(--animation-time-normal) var(--menu-animation-timing-function);
 
     // On large screen the header is not sticky but within the content that's why we align the inner menu start
     box-sizing: border-box;
 
-    &.sticky {
-      // On large screen the menu can be always open
-      @include media.min-width(large) {
-        width: var(--menu-width);
-        margin-left: 0;
-      }
-    }
-
-    // On smaller screen the menu is open on demand
-    &.open {
-      width: var(--menu-width);
-      margin-left: var(--padding);
-    }
-
-    @include media.min-width(large) {
-      padding-top: var(--padding-8x);
-    }
-
+    // Hide scrollbar for IE, Edge, and Firefox
+    -ms-overflow-style: none; // IE and Edge
+    scrollbar-width: none; // Firefox
     // Hide scrollbar for webkit
     &::-webkit-scrollbar {
       display: none;
     }
-    // Hide scrollbar for IE, Edge, and Firefox
-    -ms-overflow-style: none; // IE and Edge
-    scrollbar-width: none; // Firefox
+
+    @include media.min-width(large) {
+      padding-top: var(--padding-4x);
+    }
+  }
+
+  .logo-nns {
+    color: var(--menu-color);
+    margin-top: 1em;
+    @include media.min-width(large) {
+      padding-top: var(--padding);
+    }
+  }
+
+  .bottom-logo {
+    display: block;
+    padding: var(--padding-3x) 0;
+
+    color: var(--menu-color);
+    z-index: var(--menu-z-index);
+    text-align: center;
   }
 </style>
