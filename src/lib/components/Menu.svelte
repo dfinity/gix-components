435c6a09bce57865bd663de30f73143cfdeab84c
Cosku Cinkilic <cosku.cinkilic@dfinity.org>
2024-10-08 11:27:30 +0200

    Update menu

--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -5,6 +5,9 @@
   import IconBack from "$lib/icons/IconBack.svelte";
   import { menuCollapsed, menuStore } from "$lib/stores/menu.store";
   import { i18n } from "$lib/stores/i18n";
+  import { nonNullish } from "@dfinity/utils";
+  import { fade } from "svelte/transition";
+  import { themeStore } from "$lib/stores/theme.store";
 
   export let sticky = true;
 
@@ -12,10 +15,13 @@
 </script>
 
 <div role="menu" class:open={$layoutMenuOpen}>
-  <MenuBackground>
-    <slot name="logo" slot="logo" />
-    <slot name="oneliner" slot="oneliner" />
-  </MenuBackground>
+  <div
+    class="logo-nns logo"
+    class:open={$layoutMenuOpen}
+    class:menu-open={!$menuCollapsed}
+  >
+    <slot name="logo" />
+  </div>
 
   <div
     class="inner"
@@ -29,6 +35,17 @@
   >
     <slot />
   </div>
+
+  {#if nonNullish($themeStore)}
+    <div
+      in:fade
+      class="bottom-logo logo"
+      class:open={$layoutMenuOpen}
+      class:menu-open={!$menuCollapsed}
+    >
+      <slot name="oneliner" />
+    </div>
+  {/if}
 
   <button
     class="menu-collapse icon-only"
@@ -56,73 +73,117 @@
 
     --menu-logo-height: 65px;
     --menu-stack: 1em;
+    --inner-menu-padding-left: var(--padding);
 
-    padding-top: calc(
-      var(--menu-logo-height) + var(--padding-4x) + var(--header-offset, 0px)
-    );
+    padding-top: var(--header-offset, 0px);
 
     position: relative;
 
     // Shift the menu on large screen e.g. if a banner is displayed
     @include media.min-width(large) {
-      padding: calc(
-          var(--menu-logo-height) + var(--padding-3x) +
-            var(--header-offset, 0px) - var(--menu-selection-outer-radius)
-        )
-        var(--padding-2x) 0;
-      // remove extra space because of menu selection touches the edge
-      padding-right: 0;
-    }
-  }
-
-  .inner {
-    display: flex;
-    flex-direction: column;
-    flex-grow: 1;
-    gap: var(--padding-0_5x);
-
-    // More space for menu selection touches the edge;
-    // otherwise the first selected menu entry would be cut off in mobile view.
-    padding-top: var(--menu-selection-outer-radius);
-
-    width: 0;
-    max-width: 100vw;
-    flex-grow: 1;
-
-    overflow-y: auto;
-    margin-left: -100%;
-
-    transition:
-      margin-left var(--animation-time-normal)
-        var(--menu-animation-timing-function),
-      width var(--animation-time-normal) var(--menu-animation-timing-function);
-    // On large screen the header is not sticky but within the content that's why we align the inner menu start
-    box-sizing: border-box;
-
-    // Hide scrollbar for IE, Edge, and Firefox
-    -ms-overflow-style: none; // IE and Edge
-    scrollbar-width: none; // Firefox
-    // Hide scrollbar for webkit
-    &::-webkit-scrollbar {
-      display: none;
+      padding-top: calc(var(--padding-4x) + var(--header-offset, 0px));
+      --inner-menu-padding-left: var(--padding-2x);
     }
 
-    &.sticky {
-      // On large screen the menu can be always open
-      @include media.min-width(large) {
+    .inner {
+      display: flex;
+      flex-direction: column;
+      flex-grow: 1;
+      gap: var(--padding-0_5x);
+
+      // More space for menu selection touches the edge;
+      // otherwise the first selected menu entry would be cut off in mobile view.
+      padding-top: calc(var(--menu-selection-outer-radius) + var(--padding-4x));
+
+      width: 0;
+      max-width: 100vw;
+      flex-grow: 1;
+
+      overflow-y: auto;
+      margin-left: -100%;
+
+      transition:
+        margin-left var(--animation-time-normal)
+          var(--menu-animation-timing-function),
+        width var(--animation-time-normal) var(--menu-animation-timing-function);
+      // On large screen the header is not sticky but within the content that's why we align the inner menu start
+      box-sizing: border-box;
+
+      // Hide scrollbar for IE, Edge, and Firefox
+      -ms-overflow-style: none; // IE and Edge
+      scrollbar-width: none; // Firefox
+      // Hide scrollbar for webkit
+      &::-webkit-scrollbar {
+        display: none;
+      }
+
+      &.sticky {
+        // On large screen the menu can be always open
+        @include media.min-width(large) {
+          width: var(--menu-width);
+          margin-left: var(--inner-menu-padding-left);
+        }
+      }
+
+      // On smaller screen the menu is open on demand
+      &.open {
         width: var(--menu-width);
-        margin-left: 0;
+        margin-left: var(--inner-menu-padding-left);
+      }
+
+      @include media.min-width(large) {
+        padding-top: var(--menu-selection-outer-radius);
       }
     }
 
-    // On smaller screen the menu is open on demand
-    &.open {
-      width: var(--menu-width);
-      margin-left: var(--padding);
+    .logo-nns {
+      display: block;
+      color: var(--menu-color);
+      padding-top: var(--menu-stack);
+
+      text-align: center;
+      @include media.min-width(large) {
+        padding-top: var(--padding);
+      }
     }
 
-    @include media.min-width(large) {
-      padding-top: var(--padding-4x);
+    .bottom-logo {
+      display: none;
+      padding: var(--padding-3x) 0;
+
+      color: var(--menu-color);
+      text-align: center;
+
+      @media (min-height: 654px) {
+        display: block;
+      }
+    }
+
+    .logo {
+      width: 0;
+      transform: translateX(calc(var(--menu-expanded-width) * -1));
+
+      transition:
+        transform var(--animation-time-normal)
+          var(--menu-animation-timing-function),
+        width var(--animation-time-normal) var(--menu-animation-timing-function);
+
+      // On smaller screen the menu is open on demand
+      &.open {
+        width: calc(
+          var(--menu-expanded-width) + var(--inner-menu-padding-left)
+        );
+        transform: translateX(0);
+      }
+
+      &.menu-open {
+        @include media.min-width(large) {
+          width: calc(
+            var(--menu-expanded-width) + var(--inner-menu-padding-left)
+          );
+          transform: translateX(0);
+        }
+      }
     }
   }
 </style>
