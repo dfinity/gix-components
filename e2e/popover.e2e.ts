d80b042bb537809c830fc498f0f3d74d530826aa
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-30 16:01:54 +0100

    test: create E2E test

--- e2e/popover.e2e.ts
+++ e2e/popover.e2e.ts
@@ -13,3 +13,15 @@ test("Should render close button", async ({ page }) => {
 
   await expect(page).toHaveScreenshot();
 });
+
+test("Should render overflowing popover", async ({ page }) => {
+  await page.goto(testUrl);
+
+  const button = page.getByTestId("popover-menu-display-overflow");
+  await button.click();
+
+  // Wait for animation
+  await page.waitForTimeout(750);
+
+  await expect(page).toHaveScreenshot();
+});
