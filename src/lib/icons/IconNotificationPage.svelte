2c0d7bc85d7e1c3cbefa3f20633b3553ca153d90
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-10-30 17:46:36 +0100

    Squashed commit of the following:
    commit 1b6e6a7339694edc74e069c553fe5a74bce87e06
    Author: Max Strasinsky <maksims.strasinskis@dfinity.org>
    Date:   Wed Oct 30 17:46:06 2024 +0100
    
        Update fixed icons

--- src/lib/icons/IconNotificationPage.svelte
+++ src/lib/icons/IconNotificationPage.svelte
@@ -7,7 +7,7 @@
 <svg
   width={size}
   height={size}
-  viewBox="0 0 149 144"
+  viewBox="0 0 144 144"
   fill="none"
   xmlns="http://www.w3.org/2000/svg"
 >
@@ -21,77 +21,29 @@
     transform="rotate(7 45.354 19.0638)"
     fill="#436FF5"
   />
-  <g filter="url(#filter0_b_101_2688)">
-    <path
-      d="M53.7796 39.1425C29.5581 42.026 12.2602 63.999 15.1436 88.2205C18.0271 112.442 40.0001 129.74 64.2216 126.857C88.4432 123.973 105.741 102 102.858 77.7785C99.9741 53.557 78.0012 36.2591 53.7796 39.1425Z"
-      fill="url(#paint0_linear_101_2688)"
-    />
-  </g>
-  <g filter="url(#filter1_b_101_2688)">
-    <path
-      d="M53.7796 39.1425C29.5581 42.026 12.2602 63.999 15.1436 88.2205C18.0271 112.442 40.0001 129.74 64.2216 126.857C88.4432 123.973 105.741 102 102.858 77.7785C99.9741 53.557 78.0012 36.2591 53.7796 39.1425Z"
-      fill="url(#paint1_linear_101_2688)"
-    />
-  </g>
   <path
-    fill-rule="evenodd"
-    clip-rule="evenodd"
-    d="M57.1411 101.012C57.4007 103.192 59.3782 104.749 61.5582 104.49C63.7381 104.23 65.2949 102.253 65.0354 100.073L62.5573 79.2566C62.2978 77.0767 60.3202 75.5199 58.1403 75.7794C55.9604 76.0389 54.4036 78.0165 54.6631 80.1964L57.1411 101.012ZM57.3813 69.4037C59.5612 69.1441 61.118 67.1666 60.8585 64.9866C60.599 62.8067 58.6215 61.2499 56.4415 61.5094L56.3889 61.5157C54.2089 61.7752 52.6521 63.7527 52.9116 65.9327C53.1712 68.1126 55.1487 69.6694 57.3287 69.4099L57.3813 69.4037Z"
+    d="M53.7796 39.1426C29.558 42.026 12.2601 63.999 15.1436 88.2205C18.0271 112.442 40 129.74 64.2216 126.857C88.4431 123.973 105.741 102 102.858 77.7785C99.9741 53.557 78.0011 36.2591 53.7796 39.1426Z"
+    fill="url(#paint0_linear_101_2688)"
+  />
+  <path
+    d="M54.4752 44.9871C33.4815 47.4863 18.4888 66.5311 20.988 87.5248C23.4872 108.518 42.532 123.511 63.5256 121.012C84.5193 118.513 99.512 99.468 97.0128 78.4743C94.5136 57.4806 75.4688 42.4879 54.4752 44.9871Z"
+    fill="url(#paint1_linear_101_2688)"
+  />
+  <path
+    d="M57.1411 101.012C57.4006 103.192 59.3781 104.749 61.5581 104.49C63.738 104.23 65.2948 102.252 65.0353 100.073L62.5572 79.2565C62.2977 77.0766 60.3202 75.5198 58.1402 75.7793C55.9603 76.0388 54.4035 78.0164 54.663 80.1963L57.1411 101.012Z"
     fill="url(#paint2_linear_101_2688)"
   />
+  <path
+    d="M57.3812 69.4035C59.5612 69.144 61.118 67.1665 60.8584 64.9865C60.5989 62.8066 58.6214 61.2498 56.4414 61.5093L56.3888 61.5155C54.2089 61.7751 52.652 63.7526 52.9116 65.9326C53.1711 68.1125 55.1486 69.6693 57.3286 69.4098L57.3812 69.4035Z"
+    fill="url(#paint3_linear_101_2688)"
+  />
   <defs>
-    <filter
-      id="filter0_b_101_2688"
-      x="-1.18552"
-      y="22.8134"
-      width="120.372"
-      height="120.372"
-      filterUnits="userSpaceOnUse"
-      color-interpolation-filters="sRGB"
-    >
-      <feFlood flood-opacity="0" result="BackgroundImageFix" />
-      <feGaussianBlur in="BackgroundImageFix" stdDeviation="8.00756" />
-      <feComposite
-        in2="SourceAlpha"
-        operator="in"
-        result="effect1_backgroundBlur_101_2688"
-      />
-      <feBlend
-        mode="normal"
-        in="SourceGraphic"
-        in2="effect1_backgroundBlur_101_2688"
-        result="shape"
-      />
-    </filter>
-    <filter
-      id="filter1_b_101_2688"
-      x="-0.372242"
-      y="23.6267"
-      width="118.745"
-      height="118.746"
-      filterUnits="userSpaceOnUse"
-      color-interpolation-filters="sRGB"
-    >
-      <feFlood flood-opacity="0" result="BackgroundImageFix" />
-      <feGaussianBlur in="BackgroundImageFix" stdDeviation="7.60092" />
-      <feComposite
-        in2="SourceAlpha"
-        operator="in"
-        result="effect1_backgroundBlur_101_2688"
-      />
-      <feBlend
-        mode="normal"
-        in="SourceGraphic"
-        in2="effect1_backgroundBlur_101_2688"
-        result="shape"
-      />
-    </filter>
     <linearGradient
       id="paint0_linear_101_2688"
-      x1="144.51"
-      y1="126.279"
-      x2="-2.22018"
-      y2="53.5137"
+      x1="-20.8008"
+      y1="29.9097"
+      x2="116.312"
+      y2="119.519"
       gradientUnits="userSpaceOnUse"
     >
       <stop stop-color="#AFC3FF" stop-opacity="0" />
@@ -99,17 +51,28 @@
     </linearGradient>
     <linearGradient
       id="paint1_linear_101_2688"
-      x1="24.5121"
-      y1="96.9107"
-      x2="152.91"
-      y2="104.531"
+      x1="-10.1663"
+      y1="36.9847"
+      x2="108.674"
+      y2="114.652"
       gradientUnits="userSpaceOnUse"
     >
-      <stop stop-color="#AFC3FF" />
+      <stop stop-color="#AFC3FF" stop-opacity="0" />
       <stop offset="1" stop-color="#1851FF" />
     </linearGradient>
     <linearGradient
       id="paint2_linear_101_2688"
+      x1="62.7759"
+      y1="87.9491"
+      x2="52.2366"
+      y2="87.6729"
+      gradientUnits="userSpaceOnUse"
+    >
+      <stop stop-color="#F9FAFF" />
+      <stop offset="1" stop-color="#D9E2FD" />
+    </linearGradient>
+    <linearGradient
+      id="paint3_linear_101_2688"
       x1="62.7759"
       y1="87.9491"
       x2="52.2366"
