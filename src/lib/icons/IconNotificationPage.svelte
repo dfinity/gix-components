c0fce8e60856b29fc06fab6a338b3bf5d9ba4f49
Max Strasinsky <98811342+mstrasinskis@users.noreply.github.com>
2024-11-04 14:46:27 +0100

    Update page icons (#519)
    # Motivation
    
    Our current page icons on the signout pages are outdated and
    incompatible with the upcoming dark theme.
    
    Demo:
    https://qsgjb-riaaa-aaaaa-aaaga-cai.mstr-ingress.devenv.dfinity.network/launchpad/
    
    # Changes
    
    - Replace outdated page icons with the latest versions from Figma (copy
    pasted from Figma + size attribures).
    - Tested manually.
    
    # Screenshots
    
    <img width="1303" alt="image"
    src="https://github.com/user-attachments/assets/5a4c213a-168a-41aa-a697-f06a7a466327">
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/lib/icons/IconNotificationPage.svelte
+++ src/lib/icons/IconNotificationPage.svelte
@@ -7,102 +7,80 @@
 <svg
   width={size}
   height={size}
-  viewBox="0 0 144 144"
+  viewBox="0 0 149 144"
   fill="none"
   xmlns="http://www.w3.org/2000/svg"
 >
   <rect
     opacity="0.2"
     x="45.354"
-    y="19.064"
+    y="19.0638"
     width="102.676"
     height="102.676"
-    rx="18.0601"
-    transform="rotate(7 45.354 19.064)"
-    fill="#7C50FF"
+    rx="18.0684"
+    transform="rotate(7 45.354 19.0638)"
+    fill="#436FF5"
   />
-  <g filter="url(#filter0_b_123_2708)">
-    <path
-      d="M53.7796 39.1425C29.5581 42.026 12.2602 63.999 15.1436 88.2205C18.0271 112.442 40.0001 129.74 64.2216 126.857C88.4432 123.973 105.741 102 102.858 77.7785C99.9741 53.557 78.0012 36.2591 53.7796 39.1425Z"
-      fill="url(#paint0_linear_123_2708)"
-    />
-  </g>
-  <g filter="url(#filter1_b_123_2708)">
-    <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
-      d="M15.1436 88.2205C12.2602 63.999 29.5581 42.026 53.7796 39.1425C78.0012 36.2591 99.9741 53.557 102.858 77.7785C105.741 102 88.4432 123.973 64.2216 126.857C40.0001 129.74 18.0271 112.442 15.1436 88.2205ZM61.558 104.49C59.3781 104.749 57.4005 103.192 57.141 101.012L54.6629 80.1964C54.4034 78.0165 55.9602 76.0389 58.1402 75.7794C60.3201 75.5199 62.2977 77.0767 62.5572 79.2566L65.0353 100.073C65.2948 102.253 63.738 104.23 61.558 104.49ZM60.8584 64.9866C61.1179 67.1665 59.5611 69.1441 57.3812 69.4036L57.3285 69.4099C55.1486 69.6694 53.171 68.1126 52.9115 65.9327C52.652 63.7527 54.2088 61.7751 56.3887 61.5156L56.4414 61.5094C58.6213 61.2499 60.5989 62.8067 60.8584 64.9866Z"
-      fill="url(#paint1_linear_123_2708)"
-    />
-  </g>
+  <path
+    d="M53.7796 39.1426C29.558 42.026 12.2601 63.999 15.1436 88.2205C18.0271 112.442 40 129.74 64.2216 126.857C88.4431 123.973 105.741 102 102.858 77.7785C99.9741 53.557 78.0011 36.2591 53.7796 39.1426Z"
+    fill="url(#paint0_linear_101_2688)"
+  />
+  <path
+    d="M54.4752 44.9871C33.4815 47.4863 18.4888 66.5311 20.988 87.5248C23.4872 108.518 42.532 123.511 63.5256 121.012C84.5193 118.513 99.512 99.468 97.0128 78.4743C94.5136 57.4806 75.4688 42.4879 54.4752 44.9871Z"
+    fill="url(#paint1_linear_101_2688)"
+  />
+  <path
+    d="M57.1411 101.012C57.4006 103.192 59.3781 104.749 61.5581 104.49C63.738 104.23 65.2948 102.252 65.0353 100.073L62.5572 79.2565C62.2977 77.0766 60.3202 75.5198 58.1402 75.7793C55.9603 76.0388 54.4035 78.0164 54.663 80.1963L57.1411 101.012Z"
+    fill="url(#paint2_linear_101_2688)"
+  />
+  <path
+    d="M57.3812 69.4035C59.5612 69.144 61.118 67.1665 60.8584 64.9865C60.5989 62.8066 58.6214 61.2498 56.4414 61.5093L56.3888 61.5155C54.2089 61.7751 52.652 63.7526 52.9116 65.9326C53.1711 68.1125 55.1486 69.6693 57.3286 69.4098L57.3812 69.4035Z"
+    fill="url(#paint3_linear_101_2688)"
+  />
   <defs>
-    <filter
-      id="filter0_b_123_2708"
-      x="-0.364359"
-      y="23.6347"
-      width="118.73"
-      height="118.73"
-      filterUnits="userSpaceOnUse"
-      color-interpolation-filters="sRGB"
-    >
-      <feFlood flood-opacity="0" result="BackgroundImageFix" />
-      <feGaussianBlur in="BackgroundImageFix" stdDeviation="7.59697" />
-      <feComposite
-        in2="SourceAlpha"
-        operator="in"
-        result="effect1_backgroundBlur_123_2708"
-      />
-      <feBlend
-        mode="normal"
-        in="SourceGraphic"
-        in2="effect1_backgroundBlur_123_2708"
-        result="shape"
-      />
-    </filter>
-    <filter
-      id="filter1_b_123_2708"
-      x="-1.17722"
-      y="22.8218"
-      width="120.355"
-      height="120.356"
-      filterUnits="userSpaceOnUse"
-      color-interpolation-filters="sRGB"
-    >
-      <feFlood flood-opacity="0" result="BackgroundImageFix" />
-      <feGaussianBlur in="BackgroundImageFix" stdDeviation="8.00341" />
-      <feComposite
-        in2="SourceAlpha"
-        operator="in"
-        result="effect1_backgroundBlur_123_2708"
-      />
-      <feBlend
-        mode="normal"
-        in="SourceGraphic"
-        in2="effect1_backgroundBlur_123_2708"
-        result="shape"
-      />
-    </filter>
     <linearGradient
-      id="paint0_linear_123_2708"
-      x1="107.45"
-      y1="116.356"
-      x2="40.9339"
-      y2="110.818"
+      id="paint0_linear_101_2688"
+      x1="-20.8008"
+      y1="29.9097"
+      x2="116.312"
+      y2="119.519"
       gradientUnits="userSpaceOnUse"
     >
-      <stop stop-color="#C3AFFF" />
-      <stop offset="1" stop-color="#7952E9" />
+      <stop stop-color="#AFC3FF" stop-opacity="0" />
+      <stop offset="1" stop-color="#1851FF" />
     </linearGradient>
     <linearGradient
-      id="paint1_linear_123_2708"
-      x1="19.5"
-      y1="54"
-      x2="107.344"
-      y2="82.8052"
+      id="paint1_linear_101_2688"
+      x1="20.7158"
+      y1="13.3455"
+      x2="112.379"
+      y2="38.0049"
       gradientUnits="userSpaceOnUse"
     >
-      <stop stop-color="#F4F1FF" />
-      <stop offset="1" stop-color="#8962FF" />
+      <stop stop-color="#C4D3FF" />
+      <stop offset="1" stop-color="#003FFF" />
+    </linearGradient>
+    <linearGradient
+      id="paint2_linear_101_2688"
+      x1="62.7759"
+      y1="87.9491"
+      x2="52.2366"
+      y2="87.6729"
+      gradientUnits="userSpaceOnUse"
+    >
+      <stop stop-color="#F9FAFF" />
+      <stop offset="1" stop-color="#D9E2FD" />
+    </linearGradient>
+    <linearGradient
+      id="paint3_linear_101_2688"
+      x1="62.7759"
+      y1="87.9491"
+      x2="52.2366"
+      y2="87.6729"
+      gradientUnits="userSpaceOnUse"
+    >
+      <stop stop-color="#F9FAFF" />
+      <stop offset="1" stop-color="#D9E2FD" />
     </linearGradient>
   </defs>
 </svg>
