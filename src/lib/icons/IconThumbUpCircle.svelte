8a27d6728bcdbf9afde4e3f80c7998f8ff87b04e
Yusef Habib Fernandez <yusef.fernandez@dfinity.org>
2024-11-07 14:28:23 +0100

    revert changes to original icons

---
+++ src/lib/icons/IconThumbUpCircle.svelte
@@ -0,0 +1,28 @@
+<!-- source: DFINITY foundation -->
+<svg
+  width="48"
+  height="48"
+  viewBox="0 0 48 48"
+  role="img"
+  aria-label="Like or Thumbs Up Icon"
+  xmlns="http://www.w3.org/2000/svg"
+  data-tid="thumb-up-circle"
+>
+  <path
+    fill="#2DB286"
+    fill-opacity=".25"
+    d="M0 24C0 10.745 10.745 0 24 0s24 10.745 24 24-10.745 24-24 24S0 37.255 0 24Z"
+  />
+  <path
+    fill="#2DB386"
+    d="m19.407 22.894 5.555-6.949a.46.46 0 0 1 .7-.022c.418.458.576 1.096.422 1.696l-.785 3.064h6.446a1.466 1.466 0 0 1 1.406 1.04c.06.2.05.382.05.618v1.36a3 3 0 0 1-.225 1.137l-2.599 6.341s-.282.69-.55.927c-.268.237-.613.369-.971.37h-7.449a2 2 0 0 1-2-2v-7.582Z"
+  />
+  <rect
+    width="3.704"
+    height="9.556"
+    x="14.799"
+    y="22.92"
+    fill="#2DB386"
+    rx=".5"
+  />
+</svg>
