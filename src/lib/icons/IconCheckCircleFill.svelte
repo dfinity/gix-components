e8aed6da62c207e8e2fad571fc50d386ba1f7830
Max Strasinsky <98811342+mstrasinskis@users.noreply.github.com>
2024-12-09 09:39:55 +0100

    New icon IconCheckCircleFill (#540)
    # Motivation
    
    A new icon is needed to display the neuron’s active rewards state.
    
    # Changes
    
    - New IconCheckCircleFill.
    
    # Screenshots
    
    <img width="178" alt="image"
    src="https://github.com/user-attachments/assets/1d356581-a399-43cf-aaea-eabf88f92b4e">

---
+++ src/lib/icons/IconCheckCircleFill.svelte
@@ -0,0 +1,20 @@
+<!-- source: DFINITY foundation -->
+<script lang="ts">
+  import { DEFAULT_ICON_SIZE } from "$lib/constants/constants";
+
+  export let size = DEFAULT_ICON_SIZE;
+</script>
+
+<svg
+  width={size}
+  height={size}
+  viewBox="0 0 20 20"
+  fill="currentColor"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    fill-rule="evenodd"
+    d="M17.5 10a7.5 7.5 0 1 1-15 0 7.5 7.5 0 0 1 15 0Zm-3.545-1.332a.75.75 0 1 0-1.035-1.086l-4.068 3.881L7.081 9.77a.75.75 0 1 0-1.037 1.085l2.29 2.187a.75.75 0 0 0 1.035 0l4.586-4.374Z"
+    clip-rule="evenodd"
+  />
+</svg>
