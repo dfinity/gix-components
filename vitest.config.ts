3ab0034917c03d0612e2745118db78aba404577b
David Dal Busco <david.dalbusco@dfinity.org>
2024-12-10 06:40:26 +0100

    build: svelte v5 (without components migration)

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,10 +2,11 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { resolve } from "path";
 import type { UserConfig } from "vite";
 import { defineConfig } from "vitest/config";
+import {svelteTesting} from "@testing-library/svelte/vite";
 
 export default defineConfig(
   (): UserConfig => ({
-    plugins: [sveltekit()],
+    plugins: [sveltekit(), svelteTesting()],
     resolve: {
       alias: [
         {
@@ -36,8 +37,6 @@ export default defineConfig(
       globals: true,
       watch: false,
       setupFiles: ["./vitest.setup.ts"],
-      // Vitest issue: https://github.com/vitest-dev/vitest/issues/2834#issuecomment-1439576110
-      alias: [{ find: /^svelte$/, replacement: "svelte/internal" }],
     },
   }),
 );
