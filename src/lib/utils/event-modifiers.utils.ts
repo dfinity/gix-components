87822c3beffadbf096cd0c42303504ef4216afc5
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-05-17 12:00:28 +0200

    jsdoc

--- src/lib/utils/event-modifiers.utils.ts
+++ src/lib/utils/event-modifiers.utils.ts
@@ -8,6 +8,13 @@
 import type { OnEventCallback } from "$lib/types/event-modifiers";
 import type { MouseEventHandler } from "svelte/elements";
 
+/**
+ * A wrapper function to stop event propagation of a mouse event before executing a callback function.
+ *
+ * @param {OnEventCallback} fn - The function to be executed after stopping the event propagation. It can be a synchronous or asynchronous function.
+ *
+ * @returns {MouseEventHandler<T extends EventTarget>} - A function that takes an event and stop its propagation, before executing the provided function.
+ */
 export const stopPropagation = <T extends EventTarget>(
   fn: OnEventCallback,
 ): MouseEventHandler<T> => {
