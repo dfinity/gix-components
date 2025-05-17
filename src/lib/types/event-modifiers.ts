ba1e5177099ada46cf8c005ae8975d764131433d
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-05-17 11:29:58 +0200

    rename

--- src/lib/types/event-modifiers.ts
+++ src/lib/types/event-modifiers.ts
@@ -1,5 +1,5 @@
 export type OnEventCallback = () => void | Promise<void>;
 
 export type OnMouseEventHandler = (
-  event?: MouseEvent & { currentTarget: EventTarget & HTMLButtonElement },
+  $event?: MouseEvent & { currentTarget: EventTarget & HTMLButtonElement },
 ) => Promise<void>;
