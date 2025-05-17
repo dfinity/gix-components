888550629b1dc9dafe49cea9448eb8527992c019
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-05-17 11:56:39 +0200

    docs

--- src/lib/utils/event-modifiers.utils.ts
+++ src/lib/utils/event-modifiers.utils.ts
@@ -1,3 +1,7 @@
+// In Svelte 5 the events do not have modifiers anymore.
+// This module contains wrapper functions of event modifiers for Svelte 5.
+// Documentation: https://svelte.dev/docs/svelte/v5-migration-guide#Event-changes-Event-modifiers
+
 import type { OnEventCallback } from "$lib/types/event-modifiers";
 import type { MouseEventHandler } from "svelte/elements";
 
