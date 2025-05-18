2171d99769272436e4eb43dfac42a835cf1e4b7a
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-05-18 09:16:05 +0200

    feat!: Migrate `Back` button to Svelte 5 (#631)
    # Motivation
    
    Migrating component Back button to Svelte 5.
    
    # Changes
    
    - Migrate events in `Back` component, using `stopPropagation` wrapper.
    - Adapt the usage in `Header` component: it will need to dispatch the
    old event `nnsBack`. We will remove it when we migrate it to Svelte 5.
    
    # Screenshots
    
    Current tests are sufficient.

--- src/lib/components/Back.svelte
+++ src/lib/components/Back.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
   import IconBack from "$lib/icons/IconBack.svelte";
   import { i18n } from "$lib/stores/i18n";
-  import { createEventDispatcher } from "svelte";
+  import { stopPropagation } from "$lib/utils/event-modifiers.utils";
+  import type { OnEventCallback } from "$lib/types/event-modifiers";
 
-  const dispatch = createEventDispatcher();
+  interface Props {
+    onBack: OnEventCallback;
+  }
+
+  let { onBack }: Props = $props();
 </script>
 
 <button
   class="back icon-only"
   data-tid="back"
-  on:click|stopPropagation={() => dispatch("nnsBack")}
+  onclick={stopPropagation(onBack)}
   aria-label={$i18n.core.back}><IconBack /></button
 >
 
