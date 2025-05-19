ce3345ebfd67eaedcb0d92d97d4176ba2b82d45c
Yusef Habib <yusef.fernandez@dfinity.org>
2025-05-19 16:57:47 +0200

    feat(icons): add eye-closed icon  (#646)
    # Motivation
    
    Introduces a new icon, `IconEyeClosed`, for use in the nns-dapp during
    the hide/show balance flow.
    
    <img width="299" alt="Screenshot 2025-05-19 at 15 51 28"
    src="https://github.com/user-attachments/assets/a227a9ca-bfc2-4e88-bae4-ac025a7b9fbf"
    />
    
    # Changes
    
    - Adds new icon

---
+++ src/lib/icons/IconEyeClosed.svelte
@@ -0,0 +1,21 @@
+<!-- source: DFINITY foundation -->
+<script lang="ts">
+  import { DEFAULT_ICON_SIZE } from "$lib/constants/constants";
+
+  export let size = `${DEFAULT_ICON_SIZE}px`;
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width={size}
+  height={size}
+  fill="none"
+  viewBox="0 0 20 20"
+  ><path
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="1.5"
+    d="m15.71 9.945 1.79 3.086M12.047 11.664l.555 3.149M7.945 11.656l-.554 3.156M4.281 9.945l-1.789 3.102M2.5 8.195c1.313 1.625 3.719 3.68 7.5 3.68s6.188-2.055 7.5-3.68"
+  /></svg
+>
