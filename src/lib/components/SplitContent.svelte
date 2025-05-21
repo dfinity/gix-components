2de25688f2ede083950075f8612593b2c6161d64
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-05-22 00:19:28 +0200

    feat: Migrate SplitContent to Svelte 5

--- src/lib/components/SplitContent.svelte
+++ src/lib/components/SplitContent.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { onDestroy } from "svelte";
+  import { onDestroy, type Snippet } from "svelte";
   import {
     layoutBottomOffset,
     layoutContentScrollY,
@@ -8,15 +8,33 @@
   import Header from "$lib/components/Header.svelte";
   import ContentBackdrop from "$lib/components/ContentBackdrop.svelte";
   import ScrollSentinel from "$lib/components/ScrollSentinel.svelte";
+  import type { OnEventCallback } from "$lib/types/event-modifiers";
 
-  export let back = false;
+  interface Props {
+    title?: Snippet;
+    toolbarEnd?: Snippet;
+    start: Snippet;
+    end: Snippet;
+    back?: boolean;
+    onBack?: OnEventCallback;
+  }
+
+  let {
+    title,
+    toolbarEnd,
+    start,
+    end,
+    back = false,
+    onBack = () => {},
+  }: Props = $props();
+
   export const resetScrollPosition = () => {
     if (scrollableElement) {
       scrollableElement.scrollTop = 0;
     }
   };
 
-  let scrollableElement: HTMLElement | undefined;
+  let scrollableElement = $state<HTMLElement | undefined>();
 
   // Same as in <Content />
   onDestroy(() => ($layoutBottomOffset = 0));
@@ -30,22 +48,24 @@
   <div class="start">
     <div class="scrollable-content-start">
       <ContentBackdrop />
-      <slot name="start" />
+      {@render start()}
     </div>
   </div>
 
   <div class="end">
-    <Header {back} on:nnsBack>
-      <slot name="title" slot="title" />
+    <Header {back} on:nnsBack={onBack}>
+      <svelte:fragment slot="title">{@render title?.()}</svelte:fragment>
 
-      <slot name="toolbar-end" slot="toolbar-end" />
+      <svelte:fragment slot="toolbar-end"
+        >{@render toolbarEnd?.()}</svelte:fragment
+      >
     </Header>
 
     <div class="scrollable-content-end" bind:this={scrollableElement}>
       <ContentBackdrop />
 
       <ScrollSentinel scrollContainer={scrollableElement} />
-      <slot name="end" />
+      {@render end()}
     </div>
   </div>
 </div>
