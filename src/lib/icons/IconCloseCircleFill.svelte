3b1403ee3e7f42276b0a79035ad86eae7d39f680
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-05-26 11:08:38 +0200

    new icon

---
+++ src/lib/icons/IconCloseCircleFill.svelte
@@ -0,0 +1,18 @@
+<!-- source: DFINITY foundation -->
+<script lang="ts">
+  import { DEFAULT_ICON_SIZE } from "$lib/constants/constants";
+
+  export let size = DEFAULT_ICON_SIZE;
+</script>
+
+<svg
+  width={size}
+  height={size}
+  viewBox="0 0 20 20"
+  fill="currentColor"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d="M10 2.32a8.25 8.25 0 1 1 0 16.5 8.25 8.25 0 0 1 0-16.5Zm3.337 4.72a.75.75 0 0 0-1.06 0L9.89 9.427 7.503 7.04a.75.75 0 0 0-1.06 1.06l2.386 2.387-2.387 2.387a.75.75 0 0 0 1.06 1.06l2.388-2.386 2.386 2.386a.75.75 0 0 0 1.06-1.06l-2.386-2.387L13.337 8.1a.75.75 0 0 0 0-1.06Z"
+  /></svg
+>
