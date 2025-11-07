7883f27501dd4cea3fa64db7166f8f644f327d56
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-11-07 12:08:26 +0100

    feat: Export `Props` of `Input` component (#735)
    # Motivation
    
    In OISY, we are building components that extend the `Input` component,
    so it makes sense that we export the `Props`, to extend them as well.

--- src/lib/components/Input.svelte
+++ src/lib/components/Input.svelte
@@ -1,42 +1,8 @@
 <script lang="ts">
   import { isNullish, nonNullish } from "@dfinity/utils";
   import Decimal from "decimal.js";
-  import { type Snippet, untrack } from "svelte";
-
-  export interface Props {
-    name: string;
-    inputType?: "icp" | "number" | "text" | "currency";
-    required?: boolean;
-    spellcheck?: boolean;
-    step?: number | "any";
-    disabled?: boolean;
-    minLength?: number;
-    max?: number;
-    value?: string | number;
-    placeholder: string;
-    testId?: string;
-    decimals?: number;
-    ignore1Password?: boolean;
-    inputElement?: HTMLInputElement;
-    autofocus?: boolean;
-    // https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/autocomplete
-    autocomplete?: "off" | "on";
-    // When forwarding slots, they always appear as true
-    // This is a known issue in Svelte
-    // https://github.com/sveltejs/svelte/issues/6059
-    // To hack this, we pass a prop to avoid showing info element when not needed
-    // Ideally, this would be calculated
-    // showInfo = $$slots.label || $$slots.end
-    showInfo?: boolean;
-    onInput?: () => void;
-    onBlur?: () => void;
-    onFocus?: () => void;
-    start?: Snippet;
-    label?: Snippet;
-    end?: Snippet;
-    innerEnd?: Snippet;
-    bottom?: Snippet;
-  }
+  import { untrack } from "svelte";
+  import type { InputProps } from "$lib/types/input";
 
   let {
     name,
@@ -64,7 +30,7 @@
     end,
     innerEnd,
     bottom,
-  }: Props = $props();
+  }: InputProps = $props();
 
   $effect(() => {
     if (inputType === "number") {
